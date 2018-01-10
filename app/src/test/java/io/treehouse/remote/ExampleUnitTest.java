8b3cd5bd2552db833c2cf61dfb2111c5ce5dba6b
lorugant <lorugant@uncc.edu>
2018-01-10 16:56:29 -0500

    Changed the package name

--- app/src/test/java/io/treehouse/remote/ExampleUnitTest.java
+++ app/src/test/java/io/treehouse/remote/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package io.treehouse.remote;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
