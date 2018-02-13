8d793580bb06b54f6eace52240e0430f43a886b8
Kyle Mathias <kylemathias@gmail.com>
2018-02-13 11:54:00 -0800

    fixing testing naming

--- app/src/androidTest/java/io/treehouses/remote/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/treehouses/remote/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.yubo.bluepi", appContext.getPackageName());
+        assertEquals("io.treehouses.remote", appContext.getPackageName());
     }
 }
