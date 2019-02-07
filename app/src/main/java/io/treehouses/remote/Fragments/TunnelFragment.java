ebb019d280e5a2371761908e36b4a21302f6c7ba
Leonard <leomaxi@outlook.com>
2019-02-06 22:56:42 -0500

    tunnel error fix

--- app/src/main/java/io/treehouses/remote/Fragments/TunnelFragment.java
+++ app/src/main/java/io/treehouses/remote/Fragments/TunnelFragment.java
@@ -1,104 +1,49 @@
 package io.treehouses.remote.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.Intent;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import io.treehouses.remote.OnFragmentInteractionListener;
-import io.treehouses.remote.R;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
 
+import io.treehouses.remote.InitialActivity;
+import io.treehouses.remote.MiscOld.Constants;
+import io.treehouses.remote.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TunnelFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TunnelFragment extends androidx.fragment.app.Fragment implements OnFragmentInteractionListener {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import static android.content.Context.WIFI_SERVICE;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class TunnelFragment extends androidx.fragment.app.Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    View view;
 
-    public TunnelFragment() {
-        // Required empty public constructor
-    }
+    InitialActivity initialActivity;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TunnelFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TunnelFragment newInstance(String param1, String param2) {
-        TunnelFragment fragment = new TunnelFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public TunnelFragment(){}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_tunnel_fragment, container, false);
+        view = inflater.inflate(R.layout.activity_tunnel_fragment, container, false);
+
+        initialActivity = new InitialActivity();
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
     }
-
-
 }
