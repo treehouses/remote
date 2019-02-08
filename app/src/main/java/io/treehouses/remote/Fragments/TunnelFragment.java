c978377f371738ed29f04c664a61e1fbfd768721
unknown <rcouture27@gmail.com>
2019-02-08 15:50:50 -0500

    Updated interface

--- app/src/main/java/io/treehouses/remote/Fragments/TunnelFragment.java
+++ app/src/main/java/io/treehouses/remote/Fragments/TunnelFragment.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,15 +15,17 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import io.treehouses.remote.R;
+import org.w3c.dom.Text;
 
-import static android.content.Context.WIFI_SERVICE;
+import io.treehouses.remote.R;
 
 public class TunnelFragment extends androidx.fragment.app.Fragment {
 
     View view;
+    Context context;
 
     public TunnelFragment(){}
 
@@ -30,8 +33,6 @@ public class TunnelFragment extends androidx.fragment.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_tunnel_fragment, container, false);
-
-        populateSpinner(view);
 
         return view;
 
@@ -43,40 +44,5 @@ public class TunnelFragment extends androidx.fragment.app.Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-    }
-
-    public void populateSpinner(View view) {
-        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.command_notice, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                String selected = parent.getItemAtPosition(pos).toString();
-               // Toast.makeText(getActivity(), "You selected %s" + selected, Toast.LENGTH_LONG).show();
-
-                switch (selected) {
-                    case "on":
-                        Toast.makeText(getActivity(), "On selected", Toast.LENGTH_LONG).show();
-                        break;
-                    case "off":
-                        Toast.makeText(getActivity(), "Off selected", Toast.LENGTH_SHORT).show();
-                        break;
-                    default:
-                        Toast.makeText(getActivity(), "Not working", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
     }
 }
