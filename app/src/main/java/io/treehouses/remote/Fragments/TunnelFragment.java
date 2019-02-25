612938167d94efb995f132130c476260af4fb24a
unknown <rcouture27@gmail.com>
2019-02-25 04:14:29 -0500

    Added copy function

--- app/src/main/java/io/treehouses/remote/Fragments/TunnelFragment.java
+++ app/src/main/java/io/treehouses/remote/Fragments/TunnelFragment.java
@@ -2,6 +2,8 @@ package io.treehouses.remote.Fragments;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.database.DataSetObserver;
@@ -14,6 +16,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -245,6 +248,18 @@ public class TunnelFragment extends androidx.fragment.app.Fragment {
             }
 
         };
+
+        mConversationView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String clickdData=(String) mConversationView.getItemAtPosition(position);
+                ClipboardManager clipboard = (ClipboardManager)
+                        getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("label", clickdData);
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(getActivity(), "Copied to clipboard: " + clickdData, Toast.LENGTH_LONG).show();
+            }
+        });
 
         mConversationArrayAdapter.registerDataSetObserver(new DataSetObserver() {
             @Override
