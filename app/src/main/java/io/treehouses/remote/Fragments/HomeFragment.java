09960a4f854ecc3ee6291ee112329b9185d0b5e2
dogi <dogi@users.noreply.github.com>
2019-03-04 13:49:13 -0500

    Update HomeFragment.java

--- app/src/main/java/io/treehouses/remote/Fragments/HomeFragment.java
+++ app/src/main/java/io/treehouses/remote/Fragments/HomeFragment.java
@@ -24,7 +24,6 @@ public class HomeFragment extends androidx.fragment.app.Fragment {
 
     public HomeFragment(){}
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
