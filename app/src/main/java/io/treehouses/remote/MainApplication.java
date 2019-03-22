9f644a966117b3e058117f55bfa5fb783c9c49a1
unknown <rcouture27@gmail.com>
2019-03-22 16:57:04 -0400

    travis build fix

---
+++ app/src/main/java/io/treehouses/remote/MainApplication.java
@@ -0,0 +1,39 @@
+package io.treehouses.remote;
+
+import android.app.Application;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.treehouses.remote.Fragments.TerminalFragment;
+
+public class MainApplication extends Application {
+
+    private static ArrayList terminalList;
+    private static boolean onResume;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        terminalList = new ArrayList();
+        Log.e("tag", "MainApplication onResume called");
+        onResume = false;
+    }
+
+    public static ArrayList getTerminalList() {
+        return terminalList;
+    }
+
+    public static boolean getOnResume() {
+        return onResume;
+    }
+
+    public static boolean setOnResume() {
+        if (onResume) {
+            return onResume = false;
+        } else {
+            return onResume = true;
+        }
+    }
+}
