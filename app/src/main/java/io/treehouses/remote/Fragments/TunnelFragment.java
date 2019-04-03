3ebd8a9f982c10b39cc27ee0d03de2e5e609b094
unknown <rcouture27@gmail.com>
2019-04-03 14:37:23 -0400

    support for TunnelFragment

--- app/src/main/java/io/treehouses/remote/Fragments/TunnelFragment.java
+++ app/src/main/java/io/treehouses/remote/Fragments/TunnelFragment.java
@@ -85,7 +85,7 @@ public class TunnelFragment extends BaseFragment {
                     String writeMessage = new String(writeBuf);
                     if (!writeMessage.contains("google.com")) {
                         Log.d(TAG, "writeMessage = " + writeMessage);
-                        mConversationArrayAdapter.add("Command:  " + writeMessage);
+                        mConversationArrayAdapter.add("\nCommand:  " + writeMessage);
                     }
                     break;
                 case Constants.MESSAGE_READ:
