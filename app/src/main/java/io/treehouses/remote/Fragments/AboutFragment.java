eb7aafa48e60fc6856e5ff71acdcfbb802727b1b
Ryan Couture <rcouture27@gmail.com>
2019-06-12 20:47:23 -0400

    about: redesign page (fixes #298) (#304)

--- app/src/main/java/io/treehouses/remote/Fragments/AboutFragment.java
+++ app/src/main/java/io/treehouses/remote/Fragments/AboutFragment.java
@@ -1,22 +1,29 @@
 package io.treehouses.remote.Fragments;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import io.treehouses.remote.R;
 
 public class AboutFragment extends androidx.fragment.app.Fragment {
 
+    private Button gitHub, images, gitter;
+    View view;
+
     public AboutFragment(){}
 
-    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view =  inflater.inflate(R.layout.activity_about_fragment, container, false);
 
+        hyperLinks();
 //        Button rpi = view.findViewById(R.id.version);
 //
 //        rpi.setOnClickListener(new View.OnClickListener() {
@@ -27,5 +34,21 @@ public class AboutFragment extends androidx.fragment.app.Fragment {
 //            }
 //        });
         return view;
+    }
+
+    private void hyperLinks() {
+        gitHub = view.findViewById(R.id.btn_github);
+        images = view.findViewById(R.id.btn_image);
+        gitter = view.findViewById(R.id.btn_gitter);
+
+       // gitHub.setClickable(true);
+        gitHub.setMovementMethod(LinkMovementMethod.getInstance());
+        gitHub.setText(Html.fromHtml("<a href='https://github.com/treehouses/remote'>GitHub</a>"));
+
+        images.setMovementMethod(LinkMovementMethod.getInstance());
+        images.setText(Html.fromHtml("<a href='http://http://download.treehouses.io'>Treehouses Images</a>"));
+
+        gitter.setMovementMethod(LinkMovementMethod.getInstance());
+        gitter.setText(Html.fromHtml("<a href='https://gitter.im/open-learning-exchange/raspberrypi'>Gitter</a>"));
     }
 }
