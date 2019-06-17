7a42f5902faba08fd525da0e4d08aa1be062af06
Ryan Couture <rcouture27@gmail.com>
2019-06-17 14:40:48 -0400

    tunnel: save list commands (fixes #318) (#321)

--- app/src/main/java/io/treehouses/remote/MainApplication.java
+++ app/src/main/java/io/treehouses/remote/MainApplication.java
@@ -11,14 +11,20 @@ import io.treehouses.remote.Fragments.TerminalFragment;
 public class MainApplication extends Application {
 
     private static ArrayList terminalList;
+    private static ArrayList tunnelList;
 
     @Override
     public void onCreate() {
         super.onCreate();
         terminalList = new ArrayList();
+        tunnelList = new ArrayList();
     }
 
     public static ArrayList getTerminalList() {
         return terminalList;
+    }
+
+    public static ArrayList getTunnelList() {
+        return tunnelList;
     }
 }
