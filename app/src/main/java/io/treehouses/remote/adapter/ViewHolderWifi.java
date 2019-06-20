44ad8ed949f712694c059cf1c05512e5032935c1
Ryan Couture <rcouture27@gmail.com>
2019-06-19 22:41:35 -0400

    network: add toast messages (fixes #329) (#330)

--- app/src/main/java/io/treehouses/remote/adapter/ViewHolderWifi.java
+++ app/src/main/java/io/treehouses/remote/adapter/ViewHolderWifi.java
@@ -1,6 +1,8 @@
 package io.treehouses.remote.adapter;
 
+import android.content.Context;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -10,7 +12,7 @@ import io.treehouses.remote.callback.HomeInteractListener;
 public class ViewHolderWifi {
     public TextInputEditText etSsid, etPassword;
 
-    public ViewHolderWifi(View v, final HomeInteractListener listener) {
+    public ViewHolderWifi(View v, final HomeInteractListener listener, final Context context) {
         etSsid = v.findViewById(R.id.et_ssid);
         etPassword = v.findViewById(R.id.et_password);
         v.findViewById(R.id.btn_start_config).setOnClickListener(new View.OnClickListener() {
@@ -19,6 +21,7 @@ public class ViewHolderWifi {
                 String ssid = etSsid.getText().toString();
                 String password = etPassword.getText().toString();
                 listener.sendMessage(String.format("treehouses wifi \"%s\" \"%s\"", ssid, password));
+                Toast.makeText(context, "Connecting...", Toast.LENGTH_LONG).show();
             }
         });
     }
