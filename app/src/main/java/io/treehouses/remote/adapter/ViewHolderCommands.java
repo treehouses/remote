71e80c2459bbc3150e5e0890948a5c75c5f4713a
Ryan Couture <rcouture27@gmail.com>
2019-07-17 22:32:30 -0400

    terminal: add vnc to cmd list (fixes #401) (#402)

--- app/src/main/java/io/treehouses/remote/adapter/ViewHolderCommands.java
+++ app/src/main/java/io/treehouses/remote/adapter/ViewHolderCommands.java
@@ -9,12 +9,7 @@ import io.treehouses.remote.callback.HomeInteractListener;
 
 class ViewHolderCommands {
 
-    private Button changePass;
-    private Button treehouses;
-    private Button detect;
-    private Button docker;
-    private Button expand;
-    private Button clear;
+    private Button changePass, treehouses, detect, docker, expand, vncOn, vncStatus, clear;
 
     ViewHolderCommands(View v, final HomeInteractListener listener) {
         initializeButtons(v);
@@ -24,11 +19,13 @@ class ViewHolderCommands {
         detect.setOnClickListener(v13 -> listener.sendMessage("treehouses detectrpi"));
         docker.setOnClickListener(v12 -> listener.sendMessage("docker ps"));
         expand.setOnClickListener(v1 -> listener.sendMessage("treehouses expandfs"));
+        vncOn.setOnClickListener(v16 -> listener.sendMessage("treehouse vnc on"));
+        vncStatus.setOnClickListener(v17 -> listener.sendMessage("treehouses vnc"));
         clear.setOnClickListener(v1 -> {
             MainApplication.getTerminalList().clear();
             TerminalFragment.getInstance().getmConversationArrayAdapter().notifyDataSetChanged();
         });
-    }
+}
 
     private void initializeButtons(View v) {
         changePass = v.findViewById(R.id.btnChangePass);
@@ -36,6 +33,8 @@ class ViewHolderCommands {
         detect = v.findViewById(R.id.btnTreehousesDetect);
         docker = v.findViewById(R.id.btnDocker);
         expand = v.findViewById(R.id.btnExpand);
+        vncOn = v.findViewById(R.id.btnVncOn);
+        vncStatus = v.findViewById(R.id.btnVncStatus);
         clear = v.findViewById(R.id.btnClear);
     }
 }
