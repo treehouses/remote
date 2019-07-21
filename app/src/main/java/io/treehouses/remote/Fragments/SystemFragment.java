624eaa6f244f291266f71df0cffab8dbfee2524d
Roshan Rijal <rijalroshan94@gmail.com>
2019-07-22 01:27:41 +0545

    system: open VNC intent (fixes #406) (#404)

--- app/src/main/java/io/treehouses/remote/Fragments/SystemFragment.java
+++ app/src/main/java/io/treehouses/remote/Fragments/SystemFragment.java
@@ -1,24 +1,35 @@
 package io.treehouses.remote.Fragments;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
+
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
+import java.util.List;
 
 import io.treehouses.remote.Constants;
 import io.treehouses.remote.Network.BluetoothChatService;
 import io.treehouses.remote.R;
 import io.treehouses.remote.bases.BaseFragment;
+import io.treehouses.remote.utils.Utils;
 
 public class SystemFragment extends BaseFragment {
 
@@ -33,8 +44,6 @@ public class SystemFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_system_fragment, container, false);
-
-
         ArrayList<String> list = new ArrayList<String>();
         list.add("Reboot");
         list.add("Expand File System");
@@ -42,21 +51,14 @@ public class SystemFragment extends BaseFragment {
         list.add("RPI Password Settings");
         list.add("Container");
         list.add("Upgrade CLI");
+        list.add("Open VNC");
         list.add("Open Hotspot Settings");
 
         ListView listView = view.findViewById(R.id.listView);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                getListFragment(position);
-            }
-        });
-
+        listView.setOnItemClickListener((parent, view, position, id) -> getListFragment(position));
         mChatService = listener.getChatService();
-
         return view;
     }
 
@@ -66,7 +68,6 @@ public class SystemFragment extends BaseFragment {
                 listener.sendMessage("reboot");
                 try {
                     Thread.sleep(1000);
-
                     if (mChatService.getState() != Constants.STATE_CONNECTED) {
                         Toast.makeText(getContext(), "Bluetooth Disconnected: Reboot in progress", Toast.LENGTH_LONG).show();
                     } else {
@@ -93,12 +94,42 @@ public class SystemFragment extends BaseFragment {
                 listener.sendMessage("treehouses upgrade");
                 break;
             case 6:
+                openVnc();
+            case 7:
                 openHotspotSettings();
             default:
                 Log.e("Default Network Switch", "Nothing...");
         }
     }
 
+    private void openVnc() {
+        EditText in = new EditText(getActivity());
+        in.setHint("Enter IP Address of you raspberry PI");
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("vnc://%s:5900", "192.168.1.1")));
+        List<ResolveInfo> activities = getActivity().getPackageManager().queryIntentActivities(intent, 0);
+        if (activities.size() == 0) {
+            Snackbar.make(getView(), "No VNC Client installed on you device", Snackbar.LENGTH_LONG).setAction("Install", view -> {
+                Intent intent1 = new Intent(Intent.ACTION_VIEW);
+                intent1.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.realvnc.viewer.android"));
+                startActivity(intent1);
+            }).show();
+            return;
+        }
+        new AlertDialog.Builder(getActivity()).setTitle("Open VNC Client")
+        .setView(in)
+        .setPositiveButton("Open", (dialogInterface, i) -> {
+            String ip = in.getText().toString();
+            if (TextUtils.isEmpty(ip)) {
+                Toast.makeText(getActivity(), "Invalid ip address", Toast.LENGTH_LONG).show();
+                return;
+            }
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("vnc://%s:5900", ip))));
+            } catch (Exception e) {
+            }
+        }).setNegativeButton("Dismiss", null).show();
+    }
+  
     private void openHotspotSettings() {
         final Intent intent = new Intent(Intent.ACTION_MAIN, null);
         intent.addCategory(Intent.CATEGORY_LAUNCHER);
