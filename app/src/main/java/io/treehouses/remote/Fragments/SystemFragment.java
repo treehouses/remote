5f50fb4e9d50b5e05abaae8f6c78fcad3adc13e0
Ryan Couture <rcouture27@gmail.com>
2019-07-30 20:26:10 -0400

    system: check subnet for vnc (fixes #418) (#427)

--- app/src/main/java/io/treehouses/remote/Fragments/SystemFragment.java
+++ app/src/main/java/io/treehouses/remote/Fragments/SystemFragment.java
@@ -6,10 +6,12 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
+import android.text.format.Formatter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,24 +23,27 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import com.google.android.material.snackbar.Snackbar;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import io.treehouses.remote.Constants;
 import io.treehouses.remote.Network.BluetoothChatService;
 import io.treehouses.remote.R;
 import io.treehouses.remote.bases.BaseFragment;
 
+import static android.content.Context.WIFI_SERVICE;
+
 public class SystemFragment extends BaseFragment {
 
     View view;
     private BluetoothChatService mChatService = null;
     private EditText in;
-
+    private Boolean network = true;
+    private Boolean hostname = false;
 
     public SystemFragment() { }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_system_fragment, container, false);
         ArrayList<String> list = new ArrayList<String>();
         list.add("Reboot");
@@ -57,6 +62,15 @@ public class SystemFragment extends BaseFragment {
         mChatService = listener.getChatService();
         mChatService.updateHandler(mHandler);
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.e("TAG", "onResume called");
+        listener.sendMessage("hostname -I");
+        hostname = true;
     }
 
     public void getListFragment(int position) {
@@ -181,25 +195,82 @@ public class SystemFragment extends BaseFragment {
         public void handleMessage(Message msg) {
             if (msg.what == Constants.MESSAGE_READ) {
                 String readMessage = (String) msg.obj;
+                ArrayList<Long> diff = new ArrayList<>();
+
                 Log.d("TAG", "readMessage = " + readMessage);
 
                 if (readMessage.trim().contains("true") || readMessage.trim().contains("false")) {
                     return;
                 }
 
-                prefillIp(readMessage.trim());
+                checkAndPrefilIp(readMessage, diff);
             }
         }
     };
 
-    private void prefillIp(String readMessage) {
+    private void checkAndPrefilIp(String readMessage, ArrayList<Long> diff) {
+        if (readMessage.contains(".") && hostname) {
+            checkSubnet(readMessage, diff);
+        }
+
+        ipPrefil(readMessage, diff);
+    }
+
+    private void ipPrefil(String readMessage, ArrayList<Long> diff) {
         if (readMessage.contains("ip") && !readMessage.contains("ap0")) {
-            String[] array = readMessage.split(",");
-            foreach(array);
+            if (network) {
+                prefillIp(readMessage);
+            } else {
+                Toast.makeText(getContext(), "Warning: Your RPI may be in the wrong subnet", Toast.LENGTH_LONG).show();
+                prefillIp(readMessage);
+            }
         }
     }
 
-    private void foreach(String[] array) {
+    private void checkSubnet(String readMessage, ArrayList<Long> diff) {
+        hostname = false;
+
+        WifiManager wm = (WifiManager) getContext().getApplicationContext().getSystemService(WIFI_SERVICE);
+        String deviceIp = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+
+        long deviceIpAddress = ipToLong(deviceIp);
+
+        convertIp(readMessage, deviceIpAddress, diff);
+        network = isInNetwork(diff);
+    }
+
+    private void convertIp(String readMessage, long deviceIpAddress, ArrayList<Long> diff) {
+        String[] array = readMessage.split(" ");
+
+        for (String element : array) {
+            //TODO: Need to convert IPv6 addresses to long; currently it is being skipped
+            if (element.length() <= 15) {
+                long ip = ipToLong(element);
+                diff.add(deviceIpAddress - ip);
+            }
+        }
+    }
+
+    private long ipToLong(String ipAddress) {
+        String[] ipAddressInArray = ipAddress.split("[.]");
+        long result = 0;
+
+        for (int i = 0; i < ipAddressInArray.length; i++) {
+            int power = 3 - i;
+            int ip = Integer.parseInt(ipAddressInArray[i]);
+            result += ip * Math.pow(256, power);
+        }
+        return result;
+    }
+
+    private boolean isInNetwork(ArrayList<Long> diff) {
+        Collections.sort(diff);
+        return diff.get(0) <= 256;
+    }
+
+    private void prefillIp(String readMessage) {
+        String[] array = readMessage.split(",");
+
         for (String element : array) {
             elementConditions(element);
         }
@@ -207,7 +278,6 @@ public class SystemFragment extends BaseFragment {
 
     private void elementConditions(String element) {
         if (element.contains("ip")) {
-            Log.e("TAG", "network info: " + element);
             try {
                 in.setText(element.trim().substring(4));
             } catch (Exception e) {
