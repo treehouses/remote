6274c713088d557ed7194bbb73b0db6a891f1d5d
unknown <rcouture27@gmail.com>
2019-08-21 23:05:05 -0400

    update layout

--- app/src/main/java/io/treehouses/remote/Fragments/ProfileFragment.java
+++ app/src/main/java/io/treehouses/remote/Fragments/ProfileFragment.java
@@ -31,6 +31,7 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 public class ProfileFragment extends BaseFragment {
 
     private ListView listView;
+    private MyListAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,9 +39,7 @@ public class ProfileFragment extends BaseFragment {
 
         listView = view.findViewById(R.id.listViewProfile);
 
-        ArrayList<String> list = new ArrayList<>();
-
-        MyListAdapter adapter = new MyListAdapter(getContext());
+        adapter = new MyListAdapter(getContext());
         listView.setAdapter(adapter);
 
         return view;
@@ -60,14 +59,38 @@ public class ProfileFragment extends BaseFragment {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Log.e("TAG", "onItemSelected called");
 
-                if (spinnerProfile.getSelectedItem().toString().equals("Ethernet")) {
+                if (spinnerProfile.getItemAtPosition(position).equals("Ethernet")) {
+                    MyListAdapter.setLayout(R.layout.profile_ethernet);
+                    MyListAdapter.getMyItems().clear();
+                    MyListAdapter.setMyItems("Ethernet1");
+                    MyListAdapter.setMyItems("Ethernet2");
+                    MyListAdapter.setMyItems("Ethernet3");
+                    MyListAdapter.setMyItems("Ethernet4");
 
-                } else if (spinnerProfile.getSelectedItem().equals("Wifi")) {
+                    adapter.notifyDataSetChanged();
+                } else if (spinnerProfile.getItemAtPosition(position).equals("Wifi")) {
+                    MyListAdapter.setLayout(R.layout.profile_wifi);
+                    MyListAdapter.getMyItems().clear();
+                    MyListAdapter.setMyItems("Wifi1");
+                    MyListAdapter.setMyItems("Wifi2");
+                    adapter.notifyDataSetChanged();
+                } else if (spinnerProfile.getItemAtPosition(position).equals("Hotspot")) {
+                    MyListAdapter.setLayout(R.layout.profile_hotspot);
+                    MyListAdapter.getMyItems().clear();
+                    MyListAdapter.setMyItems("Hotspot1");
+                    MyListAdapter.setMyItems("Hotspot2");
+                    MyListAdapter.setMyItems("Hotspot3");
 
-                } else if (spinnerProfile.getSelectedItem().toString().equals("Hotspot")) {
+                    adapter.notifyDataSetChanged();
+                } else if (spinnerProfile.getItemAtPosition(position).equals("Bridge")) {
+                    MyListAdapter.setLayout(R.layout.profile_bridge);
+                    MyListAdapter.getMyItems().clear();
+                    MyListAdapter.setMyItems("Bridge1");
+                    MyListAdapter.setMyItems("Bridge2");
+                    MyListAdapter.setMyItems("Bridge3");
+                    MyListAdapter.setMyItems("Bridge4");
 
-                } else if (spinnerProfile.getSelectedItem().toString().equals("Bridge")) {
-
+                    adapter.notifyDataSetChanged();
                 }
             }
 
--- app/src/main/java/io/treehouses/remote/MyListAdapter.java
+++ app/src/main/java/io/treehouses/remote/MyListAdapter.java
@@ -6,19 +6,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class MyListAdapter extends BaseAdapter {
 
     private LayoutInflater mInflater;
-    private ArrayList myItems = new ArrayList();
+    private static ArrayList myItems = new ArrayList();
+    private static int layout = R.layout.profile_ethernet;
 
     public MyListAdapter(Context context) {
         mInflater = (LayoutInflater) context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        ListItem listItem = new ListItem();
-        myItems.add(listItem.caption);
+//        myItems.add("test");
         notifyDataSetChanged();
+    }
+
+    public static void setLayout(int layout) {
+        MyListAdapter.layout = layout;
+    }
+
+    public static void setMyItems(String value) {
+        MyListAdapter.myItems.add(value);
+    }
+
+    public static ArrayList getMyItems() {
+        return myItems;
     }
 
     @Override
@@ -41,26 +54,105 @@ public class MyListAdapter extends BaseAdapter {
         ViewHolder holder;
         if (convertView == null) {
             holder = new ViewHolder();
-            convertView = mInflater.inflate(R.layout.profile_ethernet, parent, false);
-            holder.viewCaption = convertView.findViewById(R.id.editTextIp);
+            convertView = mInflater.inflate(layout, parent, false);
+            setVariables(convertView, holder, position);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
+            //setVariables(convertView, holder, position);
         }
-        holder.viewCaption.setText("test");
-        holder.viewCaption.setId(position);
-//        ((EditText)holder.viewCaption).setOnFocusChangeListener((view, b) -> {
-//            myItems.set(position, holder.viewCaption.getText().toString());
-//            notifyDataSetChanged();
-//        });
+
         return convertView;
+    }
+    private void setVariables(View convertView, ViewHolder holder, int position) {
+        switch (layout) {
+            case R.layout.profile_ethernet:
+                ethernet(holder, convertView, position);
+                break;
+            case R.layout.profile_wifi:
+                wifi(holder, convertView, position);
+                break;
+            case R.layout.profile_hotspot:
+                hotspot(holder, convertView, position);
+                break;
+            case R.layout.profile_bridge:
+                bridge(holder, convertView, position);
+                break;
+        }
+    }
+
+    private void ethernet(ViewHolder holder, View convertView, int position) {
+        holder.editText1 = convertView.findViewById(R.id.editTextIp);
+        holder.editText2 = convertView.findViewById(R.id.editTextMask);
+        holder.editText3 = convertView.findViewById(R.id.editTextGateway);
+        holder.editText4 = convertView.findViewById(R.id.editTextDns);
+
+        holder.editText1.setText(myItems.get(position).toString());
+        holder.editText1.setId(position);
+
+        holder.editText2.setText(myItems.get(position).toString());
+        holder.editText2.setId(position);
+
+        holder.editText3.setText(myItems.get(position).toString());
+        holder.editText3.setId(position);
+
+        holder.editText4.setText(myItems.get(position).toString());
+        holder.editText4.setId(position);
+    }
+
+    private void wifi(ViewHolder holder, View convertView, int position) {
+        holder.editText1 = convertView.findViewById(R.id.editTextSSID);
+        holder.editText2 = convertView.findViewById(R.id.editTextPassword);
+
+        holder.editText1.setText("testing");
+        holder.editText1.setId(position);
+
+        holder.editText2.setText("more testing");
+        holder.editText2.setId(position);
+    }
+
+    private void hotspot(ViewHolder holder, View convertView, int position) {
+        holder.editText1 = convertView.findViewById(R.id.editTextSsid);
+        holder.editText2 = convertView.findViewById(R.id.editTextPassword);
+        holder.spinner = convertView.findViewById(R.id.spinner);
+
+        holder.editText1.setText(myItems.get(position).toString());
+        holder.editText1.setId(position);
+
+        holder.editText2.setText(myItems.get(position).toString());
+        holder.editText2.setId(position);
+
+        String value = myItems.get(position).toString();
+        holder.spinner.setSelection((value.equals("internet") ? 0 : 1));
+       // holder.spinner.setId(position);
+    }
+
+    private void bridge(ViewHolder holder, View convertView, int position) {
+        holder.editText1 = convertView.findViewById(R.id.editTextSsid);
+        holder.editText2 = convertView.findViewById(R.id.editTextPassword);
+        holder.editText3 = convertView.findViewById(R.id.editTextHostpotSsid);
+        holder.editText4 = convertView.findViewById(R.id.editTextHostpotPassword);
+
+        holder.editText1.setText(myItems.get(position).toString());
+        holder.editText1.setId(position);
+
+        holder.editText2.setText(myItems.get(position).toString());
+        holder.editText2.setId(position);
+
+        holder.editText3.setText(myItems.get(position).toString());
+        holder.editText3.setId(position);
+
+        holder.editText4.setText(myItems.get(position).toString());
+        holder.editText4.setId(position);
     }
 }
 
 class ViewHolder {
-    EditText viewCaption;
+    EditText editText1;
+    EditText editText2;
+    EditText editText3;
+    EditText editText4;
+
+    Spinner spinner;
 }
 
-class ListItem {
-    String caption;
-}
