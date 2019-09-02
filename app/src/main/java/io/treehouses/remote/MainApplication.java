24723fb63d1b0a2aec06b36f1986cfc2d9905409
unknown <rcouture27@gmail.com>
2019-09-02 17:00:52 -0400

    portait only for all activities

--- app/src/main/java/io/treehouses/remote/MainApplication.java
+++ app/src/main/java/io/treehouses/remote/MainApplication.java
@@ -1,6 +1,10 @@
 package io.treehouses.remote;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -15,10 +19,35 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+               // new activity created; force its orientation to portrait
+               activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) { }
+            @Override
+            public void onActivityResumed(Activity activity) { }
+            @Override
+            public void onActivityPaused(Activity activity) { }
+            @Override
+            public void onActivityStopped(Activity activity) { }
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) { }
+            @Override
+            public void onActivityDestroyed(Activity activity) { }
+        });
+
         terminalList = new ArrayList();
         tunnelList = new ArrayList();
         commandList = new ArrayList();
     }
+
 
     public static ArrayList getTerminalList() {
         return terminalList;
