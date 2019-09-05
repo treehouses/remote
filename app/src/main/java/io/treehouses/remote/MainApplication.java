313521dfc50dc90bb2df2e9635c423ac4d983d7f
Ryan Couture <rcouture27@gmail.com>
2019-09-04 21:59:37 -0400

    portrait only mode (fixes #458) (#461)

--- app/src/main/java/io/treehouses/remote/MainApplication.java
+++ app/src/main/java/io/treehouses/remote/MainApplication.java
@@ -1,12 +1,7 @@
 package io.treehouses.remote;
 
 import android.app.Application;
-import android.util.Log;
-
 import java.util.ArrayList;
-import java.util.List;
-
-import io.treehouses.remote.Fragments.TerminalFragment;
 
 public class MainApplication extends Application {
 
