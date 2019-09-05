22caead3ed606bbb3ff9c33dd682b29542c76ebb
unknown <rcouture27@gmail.com>
2019-09-04 21:13:43 -0400

    make things better

--- app/src/main/java/io/treehouses/remote/MainApplication.java
+++ app/src/main/java/io/treehouses/remote/MainApplication.java
@@ -20,29 +20,6 @@ public class MainApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
-
-            @Override
-            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-
-               // new activity created; force its orientation to portrait
-               activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-            }
-
-            @Override
-            public void onActivityStarted(Activity activity) { }
-            @Override
-            public void onActivityResumed(Activity activity) { }
-            @Override
-            public void onActivityPaused(Activity activity) { }
-            @Override
-            public void onActivityStopped(Activity activity) { }
-            @Override
-            public void onActivitySaveInstanceState(Activity activity, Bundle outState) { }
-            @Override
-            public void onActivityDestroyed(Activity activity) { }
-        });
-
         terminalList = new ArrayList();
         tunnelList = new ArrayList();
         commandList = new ArrayList();
