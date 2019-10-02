87b0ca605d3986a2a7122a09985b94c5b7090e02
dogi <dogi@users.noreply.github.com>
2019-10-02 12:53:18 -0400

    Revert "added standard firebase analytics (fixes #478) (#479)"
    This reverts commit 3757462cf64bf9ea4cee6108501d807428b5afa6.

--- app/src/main/java/io/treehouses/remote/MainApplication.java
+++ app/src/main/java/io/treehouses/remote/MainApplication.java
@@ -1,22 +1,18 @@
 package io.treehouses.remote;
 
 import android.app.Application;
-
-import com.google.firebase.analytics.FirebaseAnalytics;
-
 import java.util.ArrayList;
 
 public class MainApplication extends Application {
 
     private static ArrayList terminalList, tunnelList, commandList;
-    public static FirebaseAnalytics mFirebaseAnalytics;
+
     @Override
     public void onCreate() {
         super.onCreate();
         terminalList = new ArrayList();
         tunnelList = new ArrayList();
         commandList = new ArrayList();
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 
     public static ArrayList getTerminalList() {
--- app/src/main/java/io/treehouses/remote/utils/Utils.java
+++ app/src/main/java/io/treehouses/remote/utils/Utils.java
@@ -3,15 +3,7 @@ package io.treehouses.remote.utils;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
-
-import com.google.firebase.analytics.FirebaseAnalytics;
-
-import java.util.UUID;
-
-import io.treehouses.remote.MainApplication;
 
 public class Utils {
     public static void copyToClipboard(Context context, String clickedData) {
@@ -23,13 +15,5 @@ public class Utils {
         ClipData clip = ClipData.newPlainText("label", clickedData);
         clipboard.setPrimaryClip(clip);
         Toast.makeText(context, "Copied to clipboard: " + clickedData, Toast.LENGTH_LONG).show();
-    }
-
-    public static void logEvent(String name, String description) {
-        Bundle params = new Bundle();
-        params.putString("device_name", name);
-        params.putString("description", description);
-        MainApplication.mFirebaseAnalytics.logEvent("bluetooth_connected", params);
-        Log.d("TIO", "logEvent: ");
     }
 }
