becb81174498debfa5cdcf24ea101249a1321764
Srihari Vishnu <srihari.vishnu@gmail.com>
2019-11-10 12:17:16 -0500

    add text to splash screen

--- app/src/main/java/io/treehouses/remote/SplashScreenActivity.java
+++ app/src/main/java/io/treehouses/remote/SplashScreenActivity.java
@@ -8,19 +8,26 @@ import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     private static final int SPLASH_TIME_OUT = 2000;
-    Animation splash;
+    Animation logoAnimation, textAnimation;
     ImageView logo;
+    TextView logoText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         logo = findViewById(R.id.splash_logo);
-        splash = AnimationUtils.loadAnimation(this, R.anim.splash_anim);
-        logo.setAnimation(splash);
+        logoAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_logo_anim);
+        logo.setAnimation(logoAnimation);
+
+        logoText = findViewById(R.id.logo_text);
+        textAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_text_anim);
+        logoText.setAnimation(textAnimation);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
