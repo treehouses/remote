da5a8dbd80e44e46fe2d31a9522746339c04f704
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-01-24 21:28:18 -0500

    fix imports

--- app/src/main/java/io/treehouses/remote/pojo/ShellTerminal.java
+++ app/src/main/java/io/treehouses/remote/pojo/ShellTerminal.java
@@ -3,14 +3,10 @@ package io.treehouses.remote.pojo;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.text.Editable;
-import android.text.Selection;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputConnection;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.AppCompatEditText;
