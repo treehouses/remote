29abb856ede488eb558f163471d2cadb4cb7f7d7
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-01-29 11:39:11 -0500

    services: docker status based buttons (fixes #595)(fixes #599) (#600)

--- app/src/main/java/io/treehouses/remote/Fragments/ServicesTabFragment.java
+++ app/src/main/java/io/treehouses/remote/Fragments/ServicesTabFragment.java
@@ -14,6 +14,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -25,27 +26,31 @@ import io.treehouses.remote.Constants;
 import io.treehouses.remote.R;
 import io.treehouses.remote.adapter.ServicesListAdapter;
 import io.treehouses.remote.bases.BaseFragment;
+import io.treehouses.remote.pojo.ServiceInfo;
 
 public class ServicesTabFragment extends BaseFragment implements AdapterView.OnItemClickListener {
 
     private View view;
     private ProgressBar progressBar;
-    private ArrayList<String> services;
-    ArrayAdapter<String> adapter;
+    private ArrayList<ServiceInfo> services;
+    ServicesListAdapter adapter;
+    private TextView tvMessage;
 
-    public ServicesTabFragment(){}
+    public ServicesTabFragment() {
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mChatService = listener.getChatService();
         mChatService.updateHandler(mHandler);
 
-        writeToRPI("treehouses remote services available");
+        writeToRPI("treehouses remote services available\n");
 
         view = inflater.inflate(R.layout.activity_services_tab_fragment, container, false);
         progressBar = view.findViewById(R.id.progress_services);
+        tvMessage = view.findViewById(R.id.tv_message);
         progressBar.setVisibility(View.VISIBLE);
-        services = new ArrayList<String>();
+        services = new ArrayList<ServiceInfo>();
 
         ListView listView = view.findViewById(R.id.listView);
         adapter = new ServicesListAdapter(getActivity(), services);
@@ -70,40 +75,74 @@ public class ServicesTabFragment extends BaseFragment implements AdapterView.OnI
             switch (msg.what) {
                 case Constants.MESSAGE_READ:
                     String output = (String) msg.obj;
-                    if (!output.isEmpty() && output.startsWith("Available")) {
-                        //Read
+                    if (output.startsWith("Usage:")) {
+                        tvMessage.setVisibility(View.VISIBLE);
+                        tvMessage.setText("Feature not available please upgrade cli version.");
                         progressBar.setVisibility(View.GONE);
-                        services.addAll(Arrays.asList(output.substring(11).split(" ")));
-                        adapter.notifyDataSetChanged();
+                    } else if (output.startsWith("Available")) {
+                        //Read
+                        tvMessage.setVisibility(View.GONE);
+                        progressBar.setVisibility(View.GONE);
+                        updateServiceList(output.substring(output.indexOf(":") + 2).split(" "), ServiceInfo.SERVICE_AVAILABLE);
+                        writeToRPI("treehouses remote services installed\n");
+                    }else{
+                        checkServiceInfo(output);
                     }
+
             }
         }
     };
 
+    private void checkServiceInfo(String output) {
+        if (output.startsWith("Installed")) {
+            updateServiceList(output.substring(output.indexOf(":") + 2).split(" "), ServiceInfo.SERVICE_INSTALLED);
+            writeToRPI("treehouses remote services running\n");
+        } else if (output.startsWith("Running")) {
+            updateServiceList(output.substring(output.indexOf(":") + 2).split(" "), ServiceInfo.SERVICE_RUNNING);
+        }
+    }
+
+    private void updateServiceList(String[] stringList, int identifier) {
+        for (String name : stringList) {
+            int a = inServiceList(name);
+            if (a >= 0) services.get(a).serviceStatus = identifier;
+            else if (name.trim().length() > 0) services.add(new ServiceInfo(name, identifier));
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    private int inServiceList(String name) {
+        for (int i = 0; i < services.size(); i++) {
+            if (services.get(i).name.equals(name)) return i;
+        }
+        return -1;
+    }
+
     private void performService(String action, String command, String name) {
-        Log.d("SERVICES", action +" "+ name);
+        Log.d("SERVICES", action + " " + name);
         Toast.makeText(getContext(), name + " " + action, Toast.LENGTH_LONG).show();
         writeToRPI(command);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String name = services.get(position).name;
         switch (view.getId()) {
             case R.id.start_service:
-                performService("Started", "treehouses services "+services.get(position) + " start", services.get(position));
+                performService("Starting", "treehouses services " + name + " start\n", name);
                 break;
             case R.id.stop_service:
-                performService("Stopped", "treehouses services "+services.get(position) + " stop", services.get(position));
+                performService("Stopping", "treehouses services " + name + " stop\n", name);
                 break;
             case R.id.install_service:
-                performService("Installed", "treehouses services "+services.get(position) + " up", services.get(position));
+                performService("Installing", "treehouses services " + name + " up\n", name);
                 break;
             case R.id.uninstall_service:
-                performService("Uninstalled", "treehouses services "+services.get(position) + " down", services.get(position));
+                performService("Uninstalling", "treehouses services " + name + " down\n", name);
                 break;
         }
+        writeToRPI("treehouses remote services available\n");
     }
-
 
 
 }
--- app/src/main/java/io/treehouses/remote/adapter/ServicesListAdapter.java
+++ app/src/main/java/io/treehouses/remote/adapter/ServicesListAdapter.java
@@ -12,11 +12,14 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 import io.treehouses.remote.R;
+import io.treehouses.remote.pojo.ServiceInfo;
 
-public class ServicesListAdapter extends ArrayAdapter<String> {
-    private ArrayList<String> data;
+public class ServicesListAdapter extends ArrayAdapter<ServiceInfo> {
+    private ArrayList<ServiceInfo> data;
     private Context context;
-    public ServicesListAdapter(Context context, ArrayList<String> services) {
+    private TextView name;
+    private Button start, stop, install, uninstall;
+    public ServicesListAdapter(Context context, ArrayList<ServiceInfo> services) {
         super(context, 0, services);
         this.data = services;
         this.context = context;
@@ -26,9 +29,11 @@ public class ServicesListAdapter extends ArrayAdapter<String> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.services_row_layout, parent, false);
         }
+        findViews(convertView);
 
-        TextView name = convertView.findViewById(R.id.service_name);
-        name.setText(data.get(position));
+        name.setText(data.get(position).name);
+
+        setStatus(data.get(position).serviceStatus);
 
         setOnClick(parent, convertView, R.id.start_service, position);
         setOnClick(parent, convertView, R.id.stop_service, position);
@@ -36,6 +41,40 @@ public class ServicesListAdapter extends ArrayAdapter<String> {
         setOnClick(parent, convertView, R.id.uninstall_service, position);
 
         return convertView;
+    }
+
+    private void findViews(View view) {
+        name = view.findViewById(R.id.service_name);
+        start = view.findViewById(R.id.start_service);
+        stop = view.findViewById(R.id.stop_service);
+        install = view.findViewById(R.id.install_service);
+        uninstall = view.findViewById(R.id.uninstall_service);
+
+    }
+
+    private void setStatus(int status) {
+        if (status == ServiceInfo.SERVICE_AVAILABLE) {
+            setButtons(false, false, true, false);
+
+            name.setTextColor(context.getResources().getColor(R.color.md_grey_600));
+        }
+        else if (status == ServiceInfo.SERVICE_INSTALLED) {
+            setButtons(true, false, false, true);
+
+            name.setTextColor(context.getResources().getColor(R.color.md_grey_600));
+        }
+        else if (status == ServiceInfo.SERVICE_RUNNING) {
+            setButtons(false, true, false, true);
+
+            name.setTextColor(context.getResources().getColor(R.color.md_green_500));
+        }
+    }
+
+    private void setButtons(boolean first, boolean second, boolean third, boolean fourth) {
+        start.setEnabled(first);
+        stop.setEnabled(second);
+        install.setEnabled(third);
+        uninstall.setEnabled(fourth);
     }
 
     private void setOnClick(ViewGroup parent, View convertView, int id, int position) {
