a46f33669ea7cc3916eefee92df0d9cfeb5ace65
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-04-22 22:44:59 -0400

    CC

--- app/src/main/java/io/treehouses/remote/Fragments/ServicesDetailsFragment.java
+++ app/src/main/java/io/treehouses/remote/Fragments/ServicesDetailsFragment.java
@@ -58,9 +58,7 @@ public class ServicesDetailsFragment extends BaseServicesFragment implements Ada
 
     private boolean scrolled = false;
 
-    public ServicesDetailsFragment(ArrayList<ServiceInfo> serviceInfos){
-        this.services = serviceInfos;
-    }
+    public ServicesDetailsFragment(ArrayList<ServiceInfo> serviceInfos){ this.services = serviceInfos; }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -98,7 +96,6 @@ public class ServicesDetailsFragment extends BaseServicesFragment implements Ada
 
                 case Constants.MESSAGE_WRITE:
                     String write_msg = new String((byte[]) msg.obj);
-                    Log.d("WRITE", write_msg);
                     break;
             }
         }
@@ -107,16 +104,12 @@ public class ServicesDetailsFragment extends BaseServicesFragment implements Ada
     private void matchOutput(String s) {
         selected =((ServiceInfo)serviceSelector.getSelectedItem());
         Log.d("Entered", "matchOutput: "+s);
-        if (s.contains("started")) {
-            selected.serviceStatus = ServiceInfo.SERVICE_RUNNING;
+        if (s.contains("started")) { selected.serviceStatus = ServiceInfo.SERVICE_RUNNING;
         } else if (s.contains("stopped and removed")) {
             selected.serviceStatus = ServiceInfo.SERVICE_AVAILABLE;
             Log.d("STOP", "matchOutput: ");
-        } else if (s.contains("stopped") && !s.contains("removed")) {
-            selected.serviceStatus = ServiceInfo.SERVICE_INSTALLED;
-        } else if (s.contains("installed")) {
-            selected.serviceStatus = ServiceInfo.SERVICE_INSTALLED;
-        }
+        } else if (s.contains("stopped") && !s.contains("removed")) { selected.serviceStatus = ServiceInfo.SERVICE_INSTALLED;
+        } else if (s.contains("installed")) { selected.serviceStatus = ServiceInfo.SERVICE_INSTALLED; }
         else {return;}
         Collections.sort(services);
         serviceCardAdapter.notifyDataSetChanged();
@@ -208,13 +201,11 @@ public class ServicesDetailsFragment extends BaseServicesFragment implements Ada
 
     private int countHeadersBefore(int position) {
         int count = 0;
-        for (int i = 0; i <= position; i++) {
-            if (services.get(i).isHeader()) count++;
-        }
+        for (int i = 0; i <= position; i++) { if (services.get(i).isHeader()) count++; }
         return count;
     }
     private void showDeleteDialog(ServiceInfo selected) {
-        AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+        new AlertDialog.Builder(getContext())
                 .setTitle("Delete " + selected.name + "?")
                 .setMessage("Are you sure you would like to delete this service? All of its data will be lost and the service must be reinstalled.")
                 .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
@@ -224,15 +215,11 @@ public class ServicesDetailsFragment extends BaseServicesFragment implements Ada
                         wait = true;
                         setScreenState(false);
                     }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
-                    }
-                })
-                .create();
-        alertDialog.show();
+                    }}).create().show();
     }
 
     private void onInstall(ServiceInfo selected) {
@@ -241,9 +228,7 @@ public class ServicesDetailsFragment extends BaseServicesFragment implements Ada
             wait = true;
             setScreenState(false);
         }
-        else if (installedOrRunning(selected)) {
-            showDeleteDialog(selected);
-        }
+        else if (installedOrRunning(selected)) { showDeleteDialog(selected); }
     }
 
     private void onStart(ServiceInfo selected) {
