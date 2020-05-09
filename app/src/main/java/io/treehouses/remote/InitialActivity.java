78cdb85e55fce469d22f41142f50e3616e21c6d7
jiaxuan <khongjiaxuan@gmail.com>
2020-05-09 13:35:23 +0800

    must connect to rpi to use new tor

--- app/src/main/java/io/treehouses/remote/InitialActivity.java
+++ app/src/main/java/io/treehouses/remote/InitialActivity.java
@@ -123,8 +123,6 @@ public class InitialActivity extends PermissionActivity
         } else {
             if (id == R.id.menu_about) {
                 openCallFragment((new AboutFragment()));
-            }else if (id == R.id.menu_tunnel2) {
-                openCallFragment(new SSHTunnelFragment());
             }else if (id == R.id.menu_home) {
                 openCallFragment(new HomeFragment());
             } else {
