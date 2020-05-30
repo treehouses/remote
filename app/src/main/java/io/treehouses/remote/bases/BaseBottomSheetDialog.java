d61306fc4d47433d6b3e5bc4cf44cbbd7e3166a9
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-05-30 01:22:07 -0400

    network: improve visibility of dialogs (#792)

--- app/src/main/java/io/treehouses/remote/bases/BaseBottomSheetDialog.java
+++ app/src/main/java/io/treehouses/remote/bases/BaseBottomSheetDialog.java
@@ -1,16 +1,27 @@
 package io.treehouses.remote.bases;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import io.treehouses.remote.R;
 import io.treehouses.remote.callback.HomeInteractListener;
 
 public class BaseBottomSheetDialog extends BottomSheetDialogFragment {
     protected Context context;
     protected HomeInteractListener listener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle);
+        return super.onCreateDialog(savedInstanceState);
+    }
 
     @Override
     public void onAttach(@NonNull Context c) {
@@ -22,4 +33,6 @@ public class BaseBottomSheetDialog extends BottomSheetDialogFragment {
             throw new ClassCastException(c.toString() + " must implement HomeInteractListener");
         }
     }
+
+
 }
