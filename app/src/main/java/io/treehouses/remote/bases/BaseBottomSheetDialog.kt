7fe33f809ae28b55add6514749f0612e7c577a19
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-06-03 21:26:07 -0400

    Update BaseBottomSheetDialog.kt

--- app/src/main/java/io/treehouses/remote/bases/BaseBottomSheetDialog.kt
+++ app/src/main/java/io/treehouses/remote/bases/BaseBottomSheetDialog.kt
@@ -1,11 +1,21 @@
 package io.treehouses.remote.bases
 
+import android.app.Dialog
 import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import io.treehouses.remote.R
 import io.treehouses.remote.callback.HomeInteractListener
 
 open class BaseBottomSheetDialog : BottomSheetDialogFragment() {
     protected lateinit var listener: HomeInteractListener
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle)
+        return super.onCreateDialog(savedInstanceState)
+    }
+
     override fun onAttach(c: Context) {
         super.onAttach(c)
         try {
