b4aa1ce08ebee9d916e02c238c91311ec1329503
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-06-05 21:38:11 -0400

    Add suppress

--- app/src/main/java/io/treehouses/remote/Fragments/HomeFragment.kt
+++ app/src/main/java/io/treehouses/remote/Fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package io.treehouses.remote.Fragments
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.ProgressDialog
 import android.bluetooth.BluetoothAdapter
@@ -294,12 +295,13 @@ class HomeFragment : BaseHomeFragment(), SetDisconnect {
     /**
      * The Handler that gets information back from the BluetoothChatService
      */
-    private val mHandler: Handler = object : Handler() {
+    private val mHandler: Handler = @SuppressLint("HandlerLeak")
+    object : Handler() {
         override fun handleMessage(msg: Message) {
             when (msg.what) {
                 Constants.MESSAGE_READ -> {
                     val output = msg.obj as String
-                    if (!output.isEmpty()) {
+                    if (output.isNotEmpty()) {
                         readMessage(output)
                     }
                 }
