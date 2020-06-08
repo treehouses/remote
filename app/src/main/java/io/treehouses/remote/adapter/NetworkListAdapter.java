88ed4ced1793ed14ebf0c1059e4eeb2ef1dc90b3
law-lin <lawrencelin101@gmail.com>
2020-06-08 15:49:35 -0400

    add breaks

--- app/src/main/java/io/treehouses/remote/adapter/NetworkListAdapter.java
+++ app/src/main/java/io/treehouses/remote/adapter/NetworkListAdapter.java
@@ -119,14 +119,19 @@ public class NetworkListAdapter extends BaseExpandableListAdapter {
         switch(layout){
             case R.layout.open_vnc:
                 new ViewHolderVnc(convertView, context, listener);
+                break;
             case R.layout.configure_tethering:
                 new ViewHolderTether(convertView, listener, context);
+                break;
             case R.layout.configure_ssh_key:
                 new ViewHolderSSHKey(convertView, context, listener);
+                break;
             case R.layout.configure_camera:
                 new ViewHolderCamera(convertView, context, listener);
+                break;
             case R.layout.configure_wificountry:
                 new ViewHolderWifiCountry(convertView, context, listener);
+                break;
         }
 
         views[i] = convertView;
