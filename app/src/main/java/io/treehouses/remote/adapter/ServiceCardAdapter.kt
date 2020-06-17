141183cad2c7bf0ad89cba60ae89d3a8ba2e66cc
Mohammad Ali <mohammadali18997@gmail.com>
2020-06-17 18:50:42 -0400

    convert ServiceCardAdapter to kotlin

---
+++ app/src/main/java/io/treehouses/remote/adapter/ServiceCardAdapter.kt
@@ -0,0 +1,42 @@
+package io.treehouses.remote.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+import io.treehouses.remote.Fragments.ServiceCardFragment
+import io.treehouses.remote.pojo.ServiceInfo
+import java.util.*
+
+class ServiceCardAdapter(fm: FragmentManager?, data: ArrayList<ServiceInfo>) : FragmentStatePagerAdapter(fm!!, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    val data: ArrayList<ServiceInfo>
+    private fun removeHeaders(data: ArrayList<ServiceInfo>): ArrayList<ServiceInfo> {
+        val tmp = ArrayList(data)
+        val iterator = tmp.iterator()
+        while (iterator.hasNext()) {
+            if (iterator.next().isHeader) iterator.remove()
+        }
+        return tmp
+    }
+
+    override fun getCount(): Int {
+        return data.size
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return ServiceCardFragment(data[position])
+    }
+
+    override fun getItemPosition(o: Any): Int {
+        return PagerAdapter.POSITION_NONE
+    }
+
+    override fun notifyDataSetChanged() {
+        Collections.sort(data)
+        super.notifyDataSetChanged()
+    }
+
+    init {
+        this.data = removeHeaders(data)
+    }
+}
