b4c2ec603c1e671774d61d5ec6a5199b5a4f97f4
law-lin <lawrencelin101@gmail.com>
2020-06-17 19:51:09 -0400

    add email validation

--- app/src/main/java/io/treehouses/remote/Fragments/DialogFragments/FeedbackDialog.kt
+++ app/src/main/java/io/treehouses/remote/Fragments/DialogFragments/FeedbackDialog.kt
@@ -1,21 +1,17 @@
 package io.treehouses.remote.Fragments.DialogFragments
 
-import android.app.AlertDialog
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import android.widget.Toast
+import android.util.Patterns
 import androidx.fragment.app.DialogFragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import io.treehouses.remote.Constants
 import io.treehouses.remote.Network.ParseDbService
-import io.treehouses.remote.R
-import io.treehouses.remote.Views.RecyclerViewClickListener
-import io.treehouses.remote.adapter.HelpAdapter
 import io.treehouses.remote.databinding.DialogFeedbackBinding
 import java.util.HashMap
 
@@ -31,8 +27,21 @@ class FeedbackDialog : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        bind.editEmail.addTextChangedListener (object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if(!Patterns.EMAIL_ADDRESS.matcher(bind.editEmail.text.toString()).matches())
+                    bind.editEmail.error = "Enter a valid email"
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+
         bind.btnSendFeedback.setOnClickListener {
-            if (notBlank()) {
+            if (notBlank() && bind.editEmail.error != null && bind.editPhoneNumber.error != null) {
                 val map = HashMap<String, String?>()
                 map["name"] = bind.editName.text.toString()
                 map["email"] = bind.editEmail.text.toString()
@@ -48,8 +57,16 @@ class FeedbackDialog : DialogFragment() {
         bind.btnCancel.setOnClickListener {
             dismiss()
         }
-
     }
+
+    private fun notBlank(): Boolean {
+        return bind.editName.text.toString().isNotBlank() &&
+                bind.editEmail.text.toString().isNotBlank() &&
+                bind.editMessage.text.toString().isNotBlank() &&
+                (bind.radioButtonBug.isChecked || bind.radioButtonSuggestion.isChecked)
+    }
+
+
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog: Dialog = super.onCreateDialog(savedInstanceState)
@@ -61,12 +78,5 @@ class FeedbackDialog : DialogFragment() {
     override fun onStart() {
         super.onStart()
         dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-    }
-
-    private fun notBlank(): Boolean{
-        return bind.editName.text.toString().isNotBlank() &&
-                bind.editEmail.text.toString().isNotBlank() &&
-                bind.editMessage.text.toString().isNotBlank() &&
-                (bind.radioButtonBug.isChecked || bind.radioButtonSuggestion.isChecked)
     }
 }
