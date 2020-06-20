567c891b0bbbaee68af47a2130dd04f8b01c6d3c
Mohammad Ali <mohammadali18997@gmail.com>
2020-06-20 01:56:41 -0400

    Update ServicesData.kt

--- app/src/main/java/io/treehouses/remote/pojo/ServicesData.kt
+++ app/src/main/java/io/treehouses/remote/pojo/ServicesData.kt
@@ -3,19 +3,13 @@ package io.treehouses.remote.pojo
 import java.io.Serializable
 import java.util.*
 
-class ServicesData : Serializable {
-    @JvmField
-    val available: List<String>? = null
-    @JvmField
-    val installed: List<String>? = null
-    @JvmField
-    val running: List<String>? = null
-    @JvmField
-    val icon: HashMap<String, String>? = null
-    @JvmField
-    val info: HashMap<String, String>? = null
-    @JvmField
-    val autorun: HashMap<String, String>? = null
+data class ServicesData(@JvmField val available: List<String>? = null,
+                        @JvmField val installed: List<String>? = null,
+                        @JvmField val running: List<String>? = null,
+                        @JvmField val icon: HashMap<String, String>? = null,
+                        @JvmField val info: HashMap<String, String>? = null,
+                        @JvmField val autorun: HashMap<String, String>? = null) : Serializable {
+
 
     fun getAvailable(): List<String?>? {
         return available
