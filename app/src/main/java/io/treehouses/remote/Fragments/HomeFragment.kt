edd995408495b952b28c15f4eeadadfe921c3766
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-06-19 23:47:00 -0400

    fix boolean check

--- app/src/main/java/io/treehouses/remote/Fragments/HomeFragment.kt
+++ app/src/main/java/io/treehouses/remote/Fragments/HomeFragment.kt
@@ -270,7 +270,7 @@ class HomeFragment : BaseHomeFragment(), SetDisconnect {
                 dismissPDialog()
                 Toast.makeText(context, "Network Not Found", Toast.LENGTH_LONG).show()
             }
-            testConnectionResult -> {
+            !testConnectionResult -> {
                 testConnectionResult = true
                 dismissTestConnection()
             }
