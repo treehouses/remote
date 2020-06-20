543046a8cda37778c73850ef0e0d563a294aef3f
Syed M. Ali <mohammadali18997@gmail.com>
2020-06-19 22:44:21 -0400

    Update app/src/main/java/io/treehouses/remote/Fragments/TunnelSSHFragment.kt
    Co-authored-by: Derek Yeh <djyeh@uci.edu>

--- app/src/main/java/io/treehouses/remote/Fragments/TunnelSSHFragment.kt
+++ app/src/main/java/io/treehouses/remote/Fragments/TunnelSSHFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import io.treehouses.remote.R
 import io.treehouses.remote.bases.BaseFragment
 import io.treehouses.remote.databinding.ActivityTunnelSshFragmentBinding
 
