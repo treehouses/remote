1fc0cd451a93e56b004edfb26c83661d49f40e52
roshan rijal <rijalroshan94@gmail.com>
2020-06-22 20:40:48 +0545

    remove git pull origin master

--- app/src/main/java/io/treehouses/remote/pojo/CommandListItem.kt
+++ app/src/main/java/io/treehouses/remote/pojo/CommandListItem.kt
@@ -1,21 +1,21 @@
 package io.treehouses.remote.pojo
 
-class CommandListItem (private var title: String, private var command:String) {
+class CommandListItem(private var title: String, private var command: String) {
 
     fun getTitle(): String {
         return title
     }
 
     fun setTitle(title: String) {
-        this.title = title!!
+        this.title = title
     }
 
     fun getCommand(): String {
         return command
     }
 
-    fun setCommand(command: String?) {
-        this.command = command!!
+    fun setCommand(command: String) {
+        this.command = command
     }
 
 }
