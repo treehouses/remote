357266efce566781b5c54ced12068d6212fcd175
Mohammad Ali <mohammadali18997@gmail.com>
2020-06-22 19:58:37 -0400

    Update NetworkListAdapter.kt

--- app/src/main/java/io/treehouses/remote/adapter/NetworkListAdapter.kt
+++ app/src/main/java/io/treehouses/remote/adapter/NetworkListAdapter.kt
@@ -78,7 +78,7 @@ class NetworkListAdapter(val context: Context, list: List<NetworkListItem>, priv
             R.layout.configure_tethering -> ViewHolderTether(convertView, listener, context)
             R.layout.configure_ssh_key -> ViewHolderSSHKey(convertView, context, listener)
             R.layout.configure_camera -> ViewHolderCamera(convertView, context, listener!!)
-            R.layout.configure_wificountry -> ViewHolderWifiCountry(convertView, context, listener)
+            R.layout.configure_wificountry -> ViewHolderWifiCountry(convertView, context, listener!!)
         }
         views[i] = convertView
         return convertView
