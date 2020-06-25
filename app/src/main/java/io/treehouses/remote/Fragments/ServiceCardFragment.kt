88486bb104b5758339c5ea075710cb3de3f5332d
roshan rijal <rijalroshan94@gmail.com>
2020-06-25 08:44:47 +0545

    reformat code

--- app/src/main/java/io/treehouses/remote/Fragments/ServiceCardFragment.kt
+++ app/src/main/java/io/treehouses/remote/Fragments/ServiceCardFragment.kt
@@ -16,9 +16,8 @@ import com.caverock.androidsvg.SVG
 import io.treehouses.remote.callback.ServiceAction
 import io.treehouses.remote.databinding.ServiceCardBinding
 import io.treehouses.remote.pojo.ServiceInfo
-import java.util.ArrayList
 
-class ServiceCardFragment() : Fragment(), View.OnClickListener {
+class ServiceCardFragment : Fragment(), View.OnClickListener {
     private var actionListener: ServiceAction? = null
     private var binding: ServiceCardBinding? = null
     private lateinit var serviceData: ServiceInfo
@@ -30,96 +29,97 @@ class ServiceCardFragment() : Fragment(), View.OnClickListener {
                 serviceData = it.getSerializable("serviceData") as ServiceInfo
         }
     }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-            binding = ServiceCardBinding.inflate(inflater, container, false)
-            binding!!.serviceInfo.movementMethod = LinkMovementMethod.getInstance()
-            binding!!.serviceInfo.isFocusable = true
-            if (!serviceData.isHeader) {
-                setServiceInfo(serviceData.info)
-                showIcon(serviceData.icon)
-                updateButtons(serviceData.serviceStatus)
-                setAutorun(serviceData.autorun)
-                binding!!.installButton.setOnClickListener(this)
-                binding!!.startButton.setOnClickListener(this)
-                binding!!.openLink.setOnClickListener(this)
-                binding!!.autorunChecked.setOnCheckedChangeListener { buttonView: CompoundButton?, isChecked: Boolean -> actionListener!!.onClickAutorun(serviceData, isChecked) }
-            }
-            return binding!!.root
+        binding = ServiceCardBinding.inflate(inflater, container, false)
+        binding!!.serviceInfo.movementMethod = LinkMovementMethod.getInstance()
+        binding!!.serviceInfo.isFocusable = true
+        if (!serviceData.isHeader) {
+            setServiceInfo(serviceData.info)
+            showIcon(serviceData.icon)
+            updateButtons(serviceData.serviceStatus)
+            setAutorun(serviceData.autorun)
+            binding!!.installButton.setOnClickListener(this)
+            binding!!.startButton.setOnClickListener(this)
+            binding!!.openLink.setOnClickListener(this)
+            binding!!.autorunChecked.setOnCheckedChangeListener { buttonView: CompoundButton?, isChecked: Boolean -> actionListener!!.onClickAutorun(serviceData, isChecked) }
         }
-
-        private fun setAutorun(autorun: String?) {
-            binding!!.autorunChecked.isChecked = autorun!!.contains("true")
-        }
-
-        private fun setButtons(started: Boolean, installed: Boolean, three: Boolean) {
-            var string1 = "Start"
-            var visibility1 = View.GONE
-
-            if (started) {
-                string1 = "Stop"
-                visibility1 = View.VISIBLE
-            }
-
-            var string2 = "Install"
-            var visibility2 = View.GONE
-            var visibility3 = View.GONE
-
-            binding!!.startButton.text = string1
-            binding!!.openLink.visibility = visibility1
-
-            if (installed) {
-                string2 = "Uninstall"
-                visibility2 = View.VISIBLE
-                visibility3 = View.VISIBLE
-            }
-
-            binding!!.installButton.text = string2
-            binding!!.startButton.visibility = visibility2
-            binding!!.autorunChecked.visibility = visibility3
-
-
-            //restart.setEnabled(three);
-        }
-
-        private fun updateButtons(statusCode: Int) {
-            when (statusCode) {
-                ServiceInfo.SERVICE_AVAILABLE -> setButtons(false, false, false)
-                ServiceInfo.SERVICE_INSTALLED -> setButtons(false, true, false)
-                ServiceInfo.SERVICE_RUNNING -> setButtons(true, true, true)
-            }
-        }
-
-        private fun showIcon(s: String?) {
-            try {
-                Log.d(serviceData.name, "showIcon:" + serviceData.icon)
-                val svg = SVG.getFromString(s)
-                val pd = PictureDrawable(svg.renderToPicture())
-                binding!!.serviceLogo.setImageDrawable(pd)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-
-        private fun setServiceInfo(s: String?) {
-            val spannableString = SpannableString(s)
-            Linkify.addLinks(spannableString, Linkify.ALL)
-            binding!!.serviceInfo.text = s
-            binding!!.serviceInfo.movementMethod = LinkMovementMethod.getInstance()
-        }
-
-        override fun onClick(v: View) {
-            if (binding!!.installButton == v) {
-                actionListener!!.onClickInstall(serviceData)
-            } else if (binding!!.startButton == v) {
-                actionListener!!.onClickStart(serviceData)
-            } else if (binding!!.openLink == v) {
-                actionListener!!.onClickLink(serviceData)
-            }
-        }
-
-        override fun onAttach(context: Context) {
-            super.onAttach(context)
-            actionListener = parentFragment as ServiceAction?
-        }
-
+        return binding!!.root
     }
+
+    private fun setAutorun(autorun: String?) {
+        binding!!.autorunChecked.isChecked = autorun!!.contains("true")
+    }
+
+    private fun setButtons(started: Boolean, installed: Boolean, three: Boolean) {
+        var string1 = "Start"
+        var visibility1 = View.GONE
+
+        if (started) {
+            string1 = "Stop"
+            visibility1 = View.VISIBLE
+        }
+
+        var string2 = "Install"
+        var visibility2 = View.GONE
+        var visibility3 = View.GONE
+
+        binding!!.startButton.text = string1
+        binding!!.openLink.visibility = visibility1
+
+        if (installed) {
+            string2 = "Uninstall"
+            visibility2 = View.VISIBLE
+            visibility3 = View.VISIBLE
+        }
+
+        binding!!.installButton.text = string2
+        binding!!.startButton.visibility = visibility2
+        binding!!.autorunChecked.visibility = visibility3
+
+
+        //restart.setEnabled(three);
+    }
+
+    private fun updateButtons(statusCode: Int) {
+        when (statusCode) {
+            ServiceInfo.SERVICE_AVAILABLE -> setButtons(false, false, false)
+            ServiceInfo.SERVICE_INSTALLED -> setButtons(false, true, false)
+            ServiceInfo.SERVICE_RUNNING -> setButtons(true, true, true)
+        }
+    }
+
+    private fun showIcon(s: String?) {
+        try {
+            Log.d(serviceData.name, "showIcon:" + serviceData.icon)
+            val svg = SVG.getFromString(s)
+            val pd = PictureDrawable(svg.renderToPicture())
+            binding!!.serviceLogo.setImageDrawable(pd)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun setServiceInfo(s: String?) {
+        val spannableString = SpannableString(s)
+        Linkify.addLinks(spannableString, Linkify.ALL)
+        binding!!.serviceInfo.text = s
+        binding!!.serviceInfo.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    override fun onClick(v: View) {
+        if (binding!!.installButton == v) {
+            actionListener!!.onClickInstall(serviceData)
+        } else if (binding!!.startButton == v) {
+            actionListener!!.onClickStart(serviceData)
+        } else if (binding!!.openLink == v) {
+            actionListener!!.onClickLink(serviceData)
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        actionListener = parentFragment as ServiceAction?
+    }
+
+}
