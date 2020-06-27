19b2d6ec835bf9e0c02d755eb5c4d36a5fdb26f1
Mohammad Ali <mohammadali18997@gmail.com>
2020-06-27 00:31:24 -0400

    Update ServicesData.kt

--- app/src/main/java/io/treehouses/remote/pojo/ServicesData.kt
+++ app/src/main/java/io/treehouses/remote/pojo/ServicesData.kt
@@ -3,36 +3,9 @@ package io.treehouses.remote.pojo
 import java.io.Serializable
 import java.util.*
 
-data class ServicesData(private val available: List<String>? = null,
-                        private val installed: List<String>? = null,
-                        private val running: List<String>? = null,
-                        private val icon: HashMap<String, String>? = null,
-                        private val info: HashMap<String, String>? = null,
-                        private val autorun: HashMap<String, String>? = null) : Serializable {
-
-
-    fun getAvailable(): List<String?>? {
-        return available
-    }
-
-    fun getInstalled(): List<String?>? {
-        return installed
-    }
-
-    fun getRunning(): List<String?>? {
-        return running
-    }
-
-    fun getIcon(): HashMap<String, String>? {
-        return icon
-    }
-
-    fun getInfo(): HashMap<String, String>? {
-        return info
-    }
-
-    fun getAutorun(): HashMap<String, String>? {
-        return autorun
-    }
-
-}
+data class ServicesData(val available: List<String> = listOf(),
+                        val installed: List<String> = listOf(),
+                        val running: List<String> = listOf(),
+                        val icon: HashMap<String, String> = hashMapOf(),
+                        val info: HashMap<String, String> = hashMapOf(),
+                        val autorun: HashMap<String, String> =hashMapOf()) : Serializable
