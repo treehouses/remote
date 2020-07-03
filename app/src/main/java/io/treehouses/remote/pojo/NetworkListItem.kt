27b170cc5aa13776f9a47d47c77fa58e2fa19398
Mohammad Ali <mohammadali18997@gmail.com>
2020-07-03 19:58:20 -0400

    network list item: convert to kotlin

---
+++ app/src/main/java/io/treehouses/remote/pojo/NetworkListItem.kt
@@ -0,0 +1,22 @@
+package io.treehouses.remote.pojo
+
+import io.treehouses.remote.R
+import java.util.*
+
+class NetworkListItem(var title: String, var layout: Int) {
+
+    companion object {
+        val systemList: List<NetworkListItem>
+            get() {
+                val systemList: MutableList<NetworkListItem> = ArrayList()
+                systemList.add(NetworkListItem("Open VNC", R.layout.open_vnc))
+                systemList.add(NetworkListItem("Configure Tethering (beta)", R.layout.configure_tethering))
+                systemList.add(NetworkListItem("Add SSH Key", R.layout.configure_ssh_key))
+                systemList.add(NetworkListItem("Toggle Camera", R.layout.configure_camera))
+                systemList.add(NetworkListItem("Wifi Country", R.layout.configure_wificountry))
+                systemList.add(NetworkListItem("Blocker Level", R.layout.configure_blocker))
+                return systemList
+            }
+    }
+
+}
