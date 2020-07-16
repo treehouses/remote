82bd47d8f4e0d611e6851d3911353b8530b228cb
law-lin <lawrencelin101@gmail.com>
2020-07-15 22:29:36 -0400

    save and show keys

--- app/src/main/java/io/treehouses/remote/Fragments/TunnelSSHFragment.kt
+++ app/src/main/java/io/treehouses/remote/Fragments/TunnelSSHFragment.kt
@@ -1,150 +1,16 @@
 package io.treehouses.remote.Fragments
 
-import android.annotation.SuppressLint
-import android.content.Context.MODE_PRIVATE
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import io.treehouses.remote.Constants
-import io.treehouses.remote.Network.BluetoothChatService
 import io.treehouses.remote.bases.BaseFragment
 import io.treehouses.remote.databinding.ActivityTunnelSshFragmentBinding
-import io.treehouses.remote.utils.RESULTS
-import io.treehouses.remote.utils.match
-import org.json.JSONException
-import org.json.JSONObject
 
 class TunnelSSHFragment : BaseFragment() {
-    override lateinit var mChatService: BluetoothChatService
-    private var jsonReceiving = false
-    private var jsonSent = false
-    private var jsonString = ""
-
     var bind: ActivityTunnelSshFragmentBinding? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         bind = ActivityTunnelSshFragmentBinding.inflate(inflater, container, false)
-
-        mChatService = listener.getChatService()
-        mChatService.updateHandler(mHandler)
-
-        bind!!.btnGetKey.setOnClickListener {
-            listener.sendMessage("treehouses remote key send")
-            jsonSend(true)
-        }
-
-        bind!!.btnSendToPi.setOnClickListener{
-            val sharedPreferences: SharedPreferences = requireContext().getSharedPreferences("SSHKeyPref", MODE_PRIVATE)
-            val publicKey: String? = sharedPreferences.getString("public_key", "")
-            val privateKey: String? = sharedPreferences.getString("private_key", "")
-            listener.sendMessage("treehouses remote key receive $publicKey $privateKey")
-        }
-
-        bind!!.btnAddSshtunnelPort.setOnClickListener {
-            val port: String = bind!!.editTextSSHTunnel.text.toString().trim()
-            if(port != "" && port.matches("^[0-9]*$".toRegex())){
-                Log.d("Port", bind!!.editTextSSHTunnel.text.toString())
-                listener.sendMessage("treehouses sshtunnel add host $port")
-            }
-            else{
-                Toast.makeText(requireContext(), "A numeric port interval is required", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        bind!!.btnRemoveAll.setOnClickListener {
-            listener.sendMessage("treehouses sshtunnel remove all")
-        }
-
         return bind!!.root
-    }
-
-    @SuppressLint("HandlerLeak")
-    private val mHandler: Handler = object : Handler() {
-        override fun handleMessage(msg: Message) {
-            when (msg.what) {
-                Constants.MESSAGE_READ -> {
-                    val readMessage = msg.obj as String
-                    match(readMessage)
-
-                    Log.d("Read Message", readMessage)
-                    if(readMessage.contains("treehouses tor ports")){
-                        listener.sendMessage("Please try again")
-                    }
-                    if(readMessage.contains("key required")){
-                        listener.sendMessage("treehouses remote key send")
-                        Toast.makeText(requireContext(), "No keys saved, retrieving keys now", Toast.LENGTH_SHORT).show()
-                    }
-                    if(readMessage.contains("Error when trying to run the command 'treehouses sshtunnel add host")){
-                        Toast.makeText(requireContext(), "Port already exists", Toast.LENGTH_SHORT).show()
-                    }
-                    if(readMessage.contains("successfully added")){
-                        Toast.makeText(requireContext(), "Port successfully added", Toast.LENGTH_SHORT).show()
-                    }
-                    if(readMessage.contains("Saved")){
-                        Toast.makeText(requireContext(), "Keys successfully saved to Pi", Toast.LENGTH_SHORT).show()
-                    }
-                    if(readMessage.contains("Removed")){
-                        Toast.makeText(requireContext(), "All ports successfully removed", Toast.LENGTH_SHORT).show()
-                    }
-                    if (readMessage.contains("unknown")) {
-                        jsonSend(false)
-                    }
-                    else{
-                        if (jsonSent) {
-                            handleJson(readMessage)
-                        }
-                        val sharedPreferences: SharedPreferences = requireContext().getSharedPreferences("SSHKeyPref", MODE_PRIVATE)
-                        val output: String? = sharedPreferences.getString("public_key", "")
-                        Log.d("public_key", output)
-                    }
-                }
-            }
-        }
-    }
-
-    private fun buildJSON() {
-        try {
-            val jsonObject = JSONObject(jsonString)
-            Log.d("JSON Object", jsonObject.toString())
-            bind!!.publicKey.text = jsonObject.toString()
-
-            val sharedPreferences: SharedPreferences = requireContext().getSharedPreferences("SSHKeyPref", MODE_PRIVATE)
-            val myEdit = sharedPreferences.edit()
-            myEdit.putString("public_key", jsonObject.getString("public_key"))
-            myEdit.putString("private_key", jsonObject.getString("private_key"))
-            myEdit.apply()
-
-        } catch (e: JSONException) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun handleJson(readMessage: String) {
-        Log.d("readMessage", readMessage)
-        val s = match(readMessage)
-        if (jsonReceiving) {
-            jsonString += readMessage
-            if (s == RESULTS.END_JSON || s == RESULTS.END_HELP) {
-                buildJSON()
-                jsonSend(false)
-            }
-        } else if (s == RESULTS.START_JSON) {
-            jsonReceiving = true
-            jsonString = readMessage.trim()
-        }
-    }
-    private fun jsonSend(sent: Boolean) {
-        jsonSent = sent
-        if (sent) {
-            bind!!.progressBar.visibility = View.VISIBLE
-        } else {
-            bind!!.progressBar.visibility = View.GONE
-            jsonReceiving = false
-        }
     }
 }
---
+++ app/src/main/java/io/treehouses/remote/adapter/ViewHolderSSHTunnelKey.kt
@@ -0,0 +1,198 @@
+package io.treehouses.remote.adapter
+
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.SharedPreferences
+import android.os.Handler
+import android.os.Message
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import io.treehouses.remote.Constants
+import io.treehouses.remote.Network.BluetoothChatService
+import io.treehouses.remote.R
+import io.treehouses.remote.callback.HomeInteractListener
+import io.treehouses.remote.utils.RESULTS
+import io.treehouses.remote.utils.match
+import org.json.JSONException
+import org.json.JSONObject
+
+class ViewHolderSSHTunnelKey internal constructor(v: View, private val c: Context, listener: HomeInteractListener) {
+    private val mChatService: BluetoothChatService = listener.getChatService()
+
+    private var jsonReceiving = false
+    private var jsonSent = false
+    private var jsonString = ""
+
+    val keys: TextView
+    val progressBar: ProgressBar
+
+    @SuppressLint("HandlerLeak")
+    private val mHandler: Handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                Constants.MESSAGE_READ -> {
+                    val readMessage = msg.obj as String
+                    match(readMessage)
+
+                    Log.d("Read Message", readMessage)
+                    if(readMessage.contains("true") || readMessage.contains("false")){
+                        listener.sendMessage("treehouses remote key send")
+                        Toast.makeText(c, "Please wait...", Toast.LENGTH_SHORT).show()
+                    }
+                    if(readMessage.contains("key required")){
+                        listener.sendMessage("treehouses remote key send")
+                        Toast.makeText(c, "No keys saved, retrieving keys now", Toast.LENGTH_SHORT).show()
+                    }
+                    if(readMessage.contains("Error when trying to run the command 'treehouses sshtunnel add host")){
+                        Toast.makeText(c, "Port already exists", Toast.LENGTH_SHORT).show()
+                    }
+                    if(readMessage.contains("successfully added")){
+                        Toast.makeText(c, "Port successfully added", Toast.LENGTH_SHORT).show()
+                    }
+                    if(readMessage.contains("Saved")){
+                        Toast.makeText(c, "Keys successfully saved to Pi", Toast.LENGTH_SHORT).show()
+                    }
+                    if(readMessage.contains("Removed")){
+                        Toast.makeText(c, "All ports successfully removed", Toast.LENGTH_SHORT).show()
+                    }
+                    if (readMessage.contains("unknown")) {
+                        jsonSend(false)
+                    }
+                    else{
+                        if (jsonSent) {
+                            handleJson(readMessage)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    init {
+        mChatService.updateHandler(mHandler)
+        val btnGetKeys = v.findViewById<Button>(R.id.btn_save_keys)
+        val btnShowKeys = v.findViewById<Button>(R.id.btn_show_keys)
+
+        keys = v.findViewById(R.id.keys)
+        progressBar = v.findViewById(R.id.progress_bar)
+        btnGetKeys.setOnClickListener {
+            listener.sendMessage("treehouses remote key send")
+            jsonSend(true)
+        }
+
+        btnShowKeys.setOnClickListener {
+            val sharedPreferences: SharedPreferences = c.getSharedPreferences("SSHKeyPref", Context.MODE_PRIVATE)
+            val storedPublicKey: String? = sharedPreferences.getString("public_key", "")
+            val storedPrivateKey: String? = sharedPreferences.getString("private_key", "")
+
+            keys.text = "Public key: " + storedPublicKey + "\nPrivate key: " + storedPrivateKey
+        }
+    }
+
+    val dialogListener = listener
+
+    private fun buildJSON() {
+        try {
+            val jsonObject = JSONObject(jsonString)
+            Log.d("JSON Object", jsonObject.toString())
+
+            val sharedPreferences: SharedPreferences = c.getSharedPreferences("SSHKeyPref", Context.MODE_PRIVATE)
+            val myEdit = sharedPreferences.edit()
+
+            val storedPublicKey: String? = sharedPreferences.getString("public_key", "")
+            val storedPrivateKey: String? = sharedPreferences.getString("private_key", "")
+
+            val piPublicKey = jsonObject.getString("public_key")
+            val piPrivateKey = jsonObject.getString("private_key")
+
+            Log.d("storedPublicKey", storedPublicKey)
+            Log.d("storedPrivateKey", storedPrivateKey)
+            Log.d("pipPublicKey", piPublicKey)
+            Log.d("piPrivateKey", piPrivateKey)
+
+            // Pi and phone keys are the same
+            if(piPublicKey == storedPublicKey && piPrivateKey == storedPrivateKey){
+                Toast.makeText(c, "The same keys are already saved in both Pi and phone", Toast.LENGTH_SHORT).show()
+            }
+            // Key exists in Pi but not phone
+            else if(!piPublicKey.isNullOrBlank() && !piPrivateKey.isNullOrBlank() && storedPublicKey.isNullOrBlank() && storedPrivateKey.isNullOrBlank()){
+                val builder = AlertDialog.Builder(c)
+                builder.setTitle("Save Key To Phone")
+                builder.setPositiveButton("Yes") { _: DialogInterface?, _: Int ->
+                    myEdit.putString("public_key", jsonObject.getString("public_key"))
+                    myEdit.putString("private_key", jsonObject.getString("private_key"))
+                    myEdit.apply()
+                    Toast.makeText(c, "Key saved to phone successfully", Toast.LENGTH_SHORT).show()
+                }.setNegativeButton("No") { dialog: DialogInterface?, _: Int ->
+                    dialog?.dismiss()
+                }
+                builder.show()
+            }
+            // Key exists in phone but not Pi
+            else if(piPublicKey.isNullOrBlank() && piPrivateKey.isNullOrBlank() && !storedPublicKey.isNullOrBlank() && !storedPrivateKey.isNullOrBlank()){
+                val builder = AlertDialog.Builder(c)
+                builder.setTitle("Save Key To Pi")
+                builder.setPositiveButton("Yes") { _: DialogInterface?, _: Int ->
+                    dialogListener.sendMessage("treehouses remote key receive $storedPublicKey $storedPrivateKey")
+                    Toast.makeText(c, "Key saved to Pi successfully", Toast.LENGTH_SHORT).show()
+                }.setNegativeButton("No") { dialog: DialogInterface?, _: Int ->
+                    dialog?.dismiss()
+                }
+                builder.show()
+            }
+            // Keys don't exist in phone or Pi
+            else if(piPublicKey.isNullOrBlank() && piPrivateKey.isNullOrBlank() && storedPublicKey.isNullOrBlank() && storedPrivateKey.isNullOrBlank()){
+                Toast.makeText(c, "No keys exist on either Pi or phone!", Toast.LENGTH_SHORT).show()
+            }
+            // Keys are different, overwrite one or cancel
+            else{
+                val builder = AlertDialog.Builder(c)
+                builder.setTitle("Overwrite On Pi or Phone")
+                builder.setMessage("There are different keys on the Pi and the phone. Would you like to overwrite the Pi's key or the phone's key?")
+                builder.setNegativeButton("Phone") { _: DialogInterface?, _: Int ->
+                    myEdit.putString("public_key", piPublicKey)
+                    myEdit.putString("private_key", piPrivateKey)
+                    myEdit.apply()
+                    Toast.makeText(c, "The phone's key has been overwritten with Pi's key successfully ", Toast.LENGTH_SHORT).show()
+                }.setPositiveButton("Pi") { _: DialogInterface?, _: Int ->
+                    dialogListener.sendMessage("treehouses remote key receive $storedPublicKey $storedPrivateKey")
+                    Toast.makeText(c, "The Pi's key has been overwritten with the phone's key successfully ", Toast.LENGTH_SHORT).show()
+                }.setNeutralButton("Cancel"){ dialog: DialogInterface?, _: Int ->
+                    dialog?.dismiss()
+                }
+                builder.show()
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun handleJson(readMessage: String) {
+        val s = match(readMessage)
+        if (jsonReceiving) {
+            jsonString += readMessage
+            if (s == RESULTS.END_JSON || s == RESULTS.END_HELP) {
+                buildJSON()
+                jsonSend(false)
+            }
+        } else if (s == RESULTS.START_JSON) {
+            jsonReceiving = true
+            jsonString = readMessage.trim()
+        }
+    }
+    private fun jsonSend(sent: Boolean) {
+        jsonSent = sent
+        if (sent) {
+            progressBar.visibility = View.VISIBLE
+        } else {
+            progressBar.visibility = View.GONE
+            jsonReceiving = false
+        }
+    }
+}
