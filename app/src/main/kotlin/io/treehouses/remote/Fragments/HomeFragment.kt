309faa9301c797093a234ea94ea976e813edb981
Derek Yeh <djyeh@uci.edu>
2020-07-17 09:03:38 -0700

    multiverse: refactor (#1109)

--- app/src/main/kotlin/io/treehouses/remote/Fragments/HomeFragment.kt
+++ app/src/main/kotlin/io/treehouses/remote/Fragments/HomeFragment.kt
@@ -18,6 +18,7 @@ import android.view.ViewGroup
 import android.widget.ExpandableListView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import io.treehouses.remote.*
 import io.treehouses.remote.Constants.REQUEST_ENABLE_BT
 import io.treehouses.remote.Fragments.DialogFragments.RPIDialogFragment
@@ -59,17 +60,19 @@ class HomeFragment : BaseHomeFragment(), SetDisconnect {
         bind.btnGetStarted.setOnClickListener {
             instance!!.checkStatusNow()
             if (instance!!.hasValidConnection()) {
-                instance!!.openCallFragment(TerminalFragment())
-                activity?.let { it.title = "Terminal" }
+                switchFragment(TerminalFragment(), "Terminal")
             } else {
-                instance!!.openCallFragment(AboutFragment())
-                activity?.let { it.title = "About" }
+                switchFragment(AboutFragment(), "About")
             }
         }
         testConnectionListener()
         return bind.root
     }
 
+    private fun switchFragment(fragment: Fragment, title: String) {
+        instance!!.openCallFragment(fragment)
+        activity?.let { it.title = title}
+    }
 
     private fun setupProfiles() {
         val profileAdapter = ProfilesListAdapter(requireContext(), listOf(*group_labels), SaveUtils.getProfiles(requireContext()))
