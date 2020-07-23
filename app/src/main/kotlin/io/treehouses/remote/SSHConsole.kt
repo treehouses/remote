5de824ecedbbecc5cb3af5022e405663ee19252b
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-07-23 00:01:51 -0400

    show connection status

--- app/src/main/kotlin/io/treehouses/remote/SSHConsole.kt
+++ app/src/main/kotlin/io/treehouses/remote/SSHConsole.kt
@@ -244,7 +244,10 @@ open class SSHConsole : AppCompatActivity(), BridgeDisconnectedListener {
         updatePromptVisible()
 
         // If we just closed the last bridge, go back to the previous activity.
-        if (bind.pager.childCount == 0) finish()
+        if (bind.pager.childCount == 0) {
+            Log.e("FINISHING SSH", "FINISH")
+            finish()
+        }
     }
 
     private fun findCurrentView(id: Int): View? {
