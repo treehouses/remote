5bccf041e42ebfb7535e46786bd6619ca4cf18ee
Derek Yeh <djyeh@uci.edu>
2020-08-02 21:18:54 -0700

    delete extra function

--- app/src/main/kotlin/io/treehouses/remote/InitialActivity.kt
+++ app/src/main/kotlin/io/treehouses/remote/InitialActivity.kt
@@ -108,21 +108,13 @@ class InitialActivity : PermissionActivity(), NavigationView.OnNavigationItemSel
             R.id.menu_network -> openCallFragment(NewNetworkFragment())
             R.id.menu_system -> openCallFragment(SystemFragment())
             R.id.menu_terminal -> openCallFragment(TerminalFragment())
-            else -> checkMore(id)
-        }
-    }
-
-    private fun checkMore(id: Int) {
-        when (id) {
             R.id.menu_services -> openCallFragment(ServicesFragment())
             R.id.menu_about -> openCallFragment(AboutFragment())
             R.id.menu_status -> openCallFragment(StatusFragment())
             R.id.menu_tunnel2 -> openCallFragment(SSHTunnelFragment())
             R.id.menu_ssh -> openCallFragment(SSHConfig())
             else -> openCallFragment(HomeFragment())
-
         }
-
     }
 
     override fun openCallFragment(f: Fragment) {
