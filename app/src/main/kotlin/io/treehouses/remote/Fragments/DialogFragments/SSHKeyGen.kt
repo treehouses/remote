649c4b65dd35f8f55988946d5741def95bb77374
Derek Yeh <djyeh@uci.edu>
2020-08-03 09:35:03 -0700

    refactor SSHKeyGen (#1270)

--- app/src/main/kotlin/io/treehouses/remote/Fragments/DialogFragments/SSHKeyGen.kt
+++ app/src/main/kotlin/io/treehouses/remote/Fragments/DialogFragments/SSHKeyGen.kt
@@ -3,37 +3,18 @@ package io.treehouses.remote.Fragments.DialogFragments
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.core.widget.addTextChangedListener
 import android.widget.SeekBar
-import android.widget.Toast
-import androidx.lifecycle.lifecycleScope
 import io.treehouses.remote.R
-import io.treehouses.remote.SSH.PubKeyUtils
-import io.treehouses.remote.SSH.beans.PubKeyBean
-import io.treehouses.remote.bases.FullScreenDialogFragment
+import io.treehouses.remote.bases.BaseSSHKeyGen
 import io.treehouses.remote.databinding.KeysDialogBinding
 import io.treehouses.remote.utils.KeyUtils
-import kotlinx.coroutines.*
-import java.security.KeyPairGenerator
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
 
-class SSHKeyGen : FullScreenDialogFragment() {
-
-    companion object {
-        val VALID_EC_SIZES = listOf(256, 384, 521)
-    }
-
-    private lateinit var bind: KeysDialogBinding
-    private lateinit var allKeys: MutableList<String>
-    private var max = 1024
-    private var min = 1024
+class SSHKeyGen: BaseSSHKeyGen() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         bind = KeysDialogBinding.inflate(inflater, container, false)
         dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
@@ -62,27 +43,30 @@ class SSHKeyGen : FullScreenDialogFragment() {
         setUpShowStrength()
     }
 
-    private fun generateKeyOnClick() {
-        val name = bind.keyNameInput.text.toString()
-        if (!validate(name)) return
-        if (bind.inBackground.isChecked) {
-            startKeyGenInBackground(
-                    name,
-                    bind.keyTypeSpinner.selectedItem as String,
-                    bind.passwordInput.text.toString(),
-                    getBitSize())
-        } else {
-            startKeyGen(
-                    name = name,
-                    algorithm = bind.keyTypeSpinner.selectedItem.toString(),
-                    password = bind.passwordInput.text.toString(),
-                    bitSize = getBitSize()
-            )
-        }
-    }
+    private fun setUpShowStrength() {
+        bind.strengthShow.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val isValid = validate(bind.keyNameInput.text.toString())
+                bind.generateKey.isEnabled = isValid
+                val strength = if (s.isNullOrEmpty()) 0 else Integer.parseInt(s.toString())
+                if (validateBitSize(strength, getSelectedAlgo())) {
+                    setStrength(strength, false)
+                }
+            }
 
-    private fun getSelectedAlgo(): String {
-        return bind.keyTypeSpinner.selectedItem as String
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+        })
+
+        bind.strengthShow.setOnFocusChangeListener { _, _ ->
+            val value = Integer.parseInt(bind.strengthShow.text.toString())
+            when {
+                value < min -> setStrength(min)
+                value > max -> setStrength(max)
+                else -> setStrength(value)
+            }
+        }
     }
 
     private fun setUpSeekBar(algorithm: String) {
@@ -125,132 +109,4 @@ class SSHKeyGen : FullScreenDialogFragment() {
 
 
     }
-
-    private fun setUpShowStrength() {
-        bind.strengthShow.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                val isValid = validate(bind.keyNameInput.text.toString())
-                bind.generateKey.isEnabled = isValid
-                val strength = if (s.isNullOrEmpty()) 0 else Integer.parseInt(s.toString())
-                if (validateBitSize(strength, getSelectedAlgo())) {
-                    setStrength(strength, false)
-                }
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-
-        })
-
-        bind.strengthShow.setOnFocusChangeListener { _, _ ->
-            val value = Integer.parseInt(bind.strengthShow.text.toString())
-            when {
-                value < min -> setStrength(min)
-                value > max -> setStrength(max)
-                else -> setStrength(value)
-            }
-        }
-    }
-
-    private fun addTextChangeListener() {
-        bind.keyNameInput.addTextChangedListener {
-            if (validate(it.toString())) {
-                bind.keyNameLayout.error = null
-                bind.generateKey.isEnabled = true
-            } else bind.generateKey.isEnabled = false
-        }
-    }
-
-    private fun validate(keyName: String): Boolean {
-        val errorMsg = when {
-            keyName.isEmpty() -> "Please enter a key nickname"
-            allKeys.contains(keyName) -> "Name already exists!"
-            else -> ""
-        }
-        if (errorMsg.isNotEmpty()) {
-            bind.keyNameLayout.error = errorMsg
-            return false
-        }
-        val intToParse = bind.strengthShow.text.toString()
-        if (intToParse.isEmpty()) return false
-        return validateBitSize(Integer.parseInt(intToParse), getSelectedAlgo())
-    }
-
-    private fun startKeyGen(name: String, algorithm: String, password: String, bitSize: Int) {
-        bind.progressBar.visibility = View.VISIBLE
-        bind.generateKey.isEnabled = false
-        bind.keyNameInput.isEnabled = false
-        bind.keyTypeSpinner.isEnabled = false
-        bind.keyStrength.isEnabled = false
-        bind.strengthShow.isEnabled = false
-        bind.inBackground.isEnabled = false
-        bind.passwordInput.isEnabled = false
-
-        lifecycleScope.launch(Dispatchers.Default) {
-            val key = generateKey(name, algorithm, password, bitSize)
-            if (isActive) {
-                withContext(Dispatchers.Main) {
-                    KeyUtils.saveKey(requireContext(), key)
-                    bind.progressBar.visibility = View.GONE
-                    Toast.makeText(requireContext(), "Key Generation Complete: $name $algorithm-$bitSize-bit", Toast.LENGTH_LONG).show()
-                    dismiss()
-                }
-            }
-        }
-    }
-
-    private fun startKeyGenInBackground(name: String, algorithm: String, password: String, bitSize: Int) {
-        requireActivity().run {
-            GlobalScope.launch(Dispatchers.Default) {
-                val key = generateKey(name, algorithm, password, bitSize)
-                if (isActive) KeyUtils.saveKey(applicationContext, key)
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(applicationContext, "Key Generation Complete: $name $algorithm-$bitSize-bit", Toast.LENGTH_LONG).show()
-                }
-            }
-        }
-        dismiss()
-    }
-
-    private suspend fun generateKey(name: String, algorithm: String, password: String, bitSize: Int) : PubKeyBean {
-        return suspendCoroutine {
-            val keyPair = KeyPairGenerator.getInstance(algorithm).apply {
-                initialize(bitSize)
-            }.generateKeyPair()
-            Log.e("GENERATED", "KEY")
-            val key = PubKeyBean(name, algorithm, PubKeyUtils.getEncodedPrivate(keyPair.private, password), keyPair.public.encoded)
-            if (password.isNotEmpty()) key.isEncrypted = true
-            it.resume(key)
-        }
-    }
-
-
-    private fun setStrength(strength: Int, updateText: Boolean = true) {
-        if (updateText) bind.strengthShow.setText(strength.toString())
-        bind.keyStrength.progress = strength - min
-    }
-
-    private fun changeECtoClosest(progress: Int) {
-        if (getSelectedAlgo() == "EC") {
-            val newProgress = when {
-                progress <= (max - min) / 3 -> 256
-                progress >= 2 * (max - min) / 3 -> 521
-                else -> 384
-            }
-            setStrength(newProgress)
-        }
-    }
-
-    private fun validateBitSize(size: Int, algorithm: String): Boolean {
-        var isValid = true
-        if (size !in min..max) isValid = false
-        else if (algorithm == "EC" && size !in VALID_EC_SIZES) isValid = false
-        else if (algorithm == "DSA" && size != 1024) isValid = false
-        return isValid
-    }
-
-    private fun getBitSize(): Int {
-        return bind.keyStrength.progress + min
-    }
-
 }
