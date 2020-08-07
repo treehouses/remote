642a1b8b9d070eff4a03d6c1da7372f382735977
Syed Ali <mohammadali18997@gmail.com>
2020-08-07 14:32:49 -0400

    settings: clear services cache (fixes #1289) (#1290)

--- app/src/main/kotlin/io/treehouses/remote/utils/SaveUtils.kt
+++ app/src/main/kotlin/io/treehouses/remote/utils/SaveUtils.kt
@@ -34,6 +34,12 @@ object SaveUtils {
         e.apply()
     }
 
+    fun removeStringList(context: Context, arrayName: String) {
+        val e = PreferenceManager.getDefaultSharedPreferences(context).edit()
+        e.remove(arrayName)
+        e.apply()
+    }
+
     fun getStringList(context: Context, arrayName: String): MutableList<String> {
         val strList: List<String>
         val prefs = PreferenceManager.getDefaultSharedPreferences(context)
