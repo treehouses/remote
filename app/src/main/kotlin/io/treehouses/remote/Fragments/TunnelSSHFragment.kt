02b73663b59efa4fc9db15a355a3e6126e1dc4fd
Ali <mohammadali18997@gmail.com>
2020-08-10 15:59:19 -0400

    Update TunnelSSHFragment.kt

--- app/src/main/kotlin/io/treehouses/remote/Fragments/TunnelSSHFragment.kt
+++ app/src/main/kotlin/io/treehouses/remote/Fragments/TunnelSSHFragment.kt
@@ -138,8 +138,7 @@ class TunnelSSHFragment : BaseFragment(), View.OnClickListener {
                 myPos = hostsPosition!!.lastIndex
             }
             Log.d("dasda", myPos.toString())
-            fun getListenerCommand(command:Int, String2:String):String{return getString(command,  String2 + " " + hostsName!![myPos].split(":")[0])}
-            listener.sendMessage(getListenerCommand(R.string.TREEHOUSES_SSHTUNNEL_REMOVE_PORT, portsName!![position].split(":".toRegex(), 2).toTypedArray()[0]))
+            listener.sendMessage(getString(R.string.TREEHOUSES_SSHTUNNEL_REMOVE_PORT,  portsName!![position].split(":".toRegex(), 2).toTypedArray()[0] + " " + hostsName!![myPos].split(":")[0]))
             addPortButton!!.text = "deleting port ....."
             portList!!.isEnabled = false
             addPortButton!!.isEnabled = false
