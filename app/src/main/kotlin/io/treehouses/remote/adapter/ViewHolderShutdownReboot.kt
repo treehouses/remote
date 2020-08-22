7f388f1888a9e7a3c112f74f40c79570432dd903
Ali <mohammadali18997@gmail.com>
2020-08-22 05:30:14 -0400

    ui adjustments

--- app/src/main/kotlin/io/treehouses/remote/adapter/ViewHolderShutdownReboot.kt
+++ app/src/main/kotlin/io/treehouses/remote/adapter/ViewHolderShutdownReboot.kt
@@ -6,13 +6,18 @@ import android.os.Handler
 import android.os.Message
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.RadioGroup
 import io.treehouses.remote.Constants
 import io.treehouses.remote.Network.BluetoothChatService
+import io.treehouses.remote.R
 import io.treehouses.remote.callback.HomeInteractListener
 
 class ViewHolderShutdownReboot internal constructor(v: View, context: Context?, listener: HomeInteractListener) {
     private var readMessage  = ""
     private val mChatService: BluetoothChatService = listener.getChatService()
+    private val shutdownBtn: Button = v.findViewById(R.id.shutdownBtn)
+    private val rebootBtn: Button = v.findViewById(R.id.rebootBtn)
 
     /**
      * The Handler that gets information back from the BluetoothChatService
@@ -30,6 +35,7 @@ class ViewHolderShutdownReboot internal constructor(v: View, context: Context?, 
 
     init {
         mChatService.updateHandler(mHandler)
+
     }
 
 
