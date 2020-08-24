846cc4ab361f871e99a619b6c776589beeb3f3b6
Ali <mohammadali18997@gmail.com>
2020-08-24 07:30:11 -0400

    Update DiscoverFragment.kt

--- app/src/main/kotlin/io/treehouses/remote/Fragments/DiscoverFragment.kt
+++ app/src/main/kotlin/io/treehouses/remote/Fragments/DiscoverFragment.kt
@@ -14,6 +14,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
 import com.parse.Parse.getApplicationContext
 import io.treehouses.remote.Constants
 import io.treehouses.remote.Fragments.DialogFragments.RPIDialogFragment
