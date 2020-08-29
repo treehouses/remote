0ca5c3c507b8e1ec34142bea4fdb8ab3b06b2a72
anastasia21112 <anad221122@gmail.com>
2020-08-28 23:55:48 -0600

    clicks

--- app/src/main/kotlin/io/treehouses/remote/Fragments/AboutFragment.kt
+++ app/src/main/kotlin/io/treehouses/remote/Fragments/AboutFragment.kt
@@ -22,7 +22,7 @@ import kotlin.concurrent.thread
 
 class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view:View = inflater.inflate(R.layout.fragment_about, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_about, container, false)
 
         setHyperlinks(view)
         setFeedback(view)
@@ -32,7 +32,7 @@ class AboutFragment : Fragment() {
         return view
     }
 
-    private fun setHyperlinks(view : View) {
+    private fun setHyperlinks(view: View) {
         val gitHub = view.findViewById<Button>(R.id.btn_github)
         val images = view.findViewById<Button>(R.id.btn_image)
         val gitter = view.findViewById<Button>(R.id.btn_gitter)
@@ -45,7 +45,7 @@ class AboutFragment : Fragment() {
 
     }
 
-    private fun setFeedback(view : View) {
+    private fun setFeedback(view: View) {
         val giveFeedback = view.findViewById<Button>(R.id.give_feedback)
 
         giveFeedback.setOnClickListener {
@@ -55,7 +55,7 @@ class AboutFragment : Fragment() {
         }
     }
 
-    private fun setVersion(view : View) {
+    private fun setVersion(view: View) {
         val version = view.findViewById<Button>(R.id.btn_version)
         version.setOnClickListener {
             var versionName = BuildConfig.VERSION_NAME
@@ -66,28 +66,20 @@ class AboutFragment : Fragment() {
         }
     }
 
-    private fun setCopyright(view : View) {
+    private fun setCopyright(view: View) {
         val tvCopyright = view.findViewById<TextView>(R.id.tv_copyright)
         val format = SimpleDateFormat("yyyy")
         tvCopyright.text = String.format(getString(R.string.copyright), format.format(Date()) + "")
     }
 
     private fun hyperLinks(view: View, url: String) {
-        var click = true
         view.setOnClickListener {
             val i = Intent(Intent.ACTION_VIEW)
             i.data = Uri.parse(url)
-            if (click) {
-                view.setBackgroundColor(resources.getColor(R.color.colorPrimaryLight))
-                click = false
-            } else {
-                view.setBackgroundColor(resources.getColor(R.color.colorPrimary))
-                click = true
-            }
             startActivity(i)
         }
 
 
-
     }
+
 }
