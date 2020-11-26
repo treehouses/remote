0d5366bd3fa1f4d8cf7094686e45dedf7ed3fc4c
anastasia21112 <anad221122@gmail.com>
2020-11-25 21:46:04 -0700

    mvvm

--- app/src/main/kotlin/io/treehouses/remote/ui/discover/DiscoverFragment.kt
+++ app/src/main/kotlin/io/treehouses/remote/ui/discover/DiscoverFragment.kt
@@ -1,12 +1,34 @@
 package io.treehouses.remote.ui.discover
 
+import android.os.Bundle
+import android.text.AutoText.getSize
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import io.treehouses.remote.bases.BaseFragment
+import io.treehouses.remote.databinding.ActivityDiscoverFragmentBinding
+import io.treehouses.remote.fragments.DiscoverFragment
 import io.treehouses.remote.interfaces.FragmentDialogInterface
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.random.Random
 
 class DiscoverFragment : BaseFragment(), FragmentDialogInterface {
 
     protected val viewModel: DiscoverViewModel by viewModels(ownerProducer = {this})
+    private lateinit var bind: ActivityDiscoverFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        bind = ActivityDiscoverFragmentBinding.inflate(inflater, container, false)
+        viewModel.onLoad()
+        mChatService = listener.getChatService()
+        mChatService.updateHandler(mHandler)
+
+        return bind.root
+    }
+
 
 
 }
--- app/src/main/kotlin/io/treehouses/remote/ui/discover/DiscoverViewModel.kt
+++ app/src/main/kotlin/io/treehouses/remote/ui/discover/DiscoverViewModel.kt
@@ -1,7 +1,69 @@
 package io.treehouses.remote.ui.discover
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import io.treehouses.remote.R
 import io.treehouses.remote.bases.FragmentViewModel
+import io.treehouses.remote.fragments.DiscoverFragment
+import io.treehouses.remote.utils.logD
+import io.treehouses.remote.utils.logE
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.random.Random
 
 class DiscoverViewModel(application: Application) : FragmentViewModel(application) {
+    val isLoading1: MutableLiveData<Boolean> = MutableLiveData()
+    val isLoading2: MutableLiveData<Boolean> = MutableLiveData()
+    val deviceContainer: MutableLiveData<Boolean> = MutableLiveData()
+    val gatewayIcon: MutableLiveData<Boolean> = MutableLiveData()
+    val swiperefresh: MutableLiveData<Boolean> = MutableLiveData()
+    val deviceList: MutableLiveData<DiscoverFragment.Device> = MutableLiveData()
+
+    fun onLoad()
+    {
+        loadBT()
+        requestNetworkInfo()
+
+
+    }
+
+    private fun requestNetworkInfo() {
+        logD("Requesting Network Information")
+        try {
+            sendMessage(getString(R.string.TREEHOUSES_DISCOVER_GATEWAY_LIST))
+            sendMessage(getString(R.string.TREEHOUSES_DISCOVER_GATEWAY))
+            sendMessage(getString(R.string.TREEHOUSES_DISCOVER_SELF))
+        } catch (e: Exception) {
+            logE("Error Requesting Network Information")
+        }
+    }
+
+    override fun onRead(output: String) {
+        super.onRead(output)
+        logD("READ = $output")
+
+        if(!addDevices(output))
+            if(!updateGatewayInfo(output))
+                updatePiInfo(output)
+
+        if (output.startsWith("Ports:")) transition()
+    }
+
+    private fun addDevices(readMessage: String): Boolean {
+        var regex = "([0-9]+\\.){3}[0-9]+\\s+([0-9A-Z]+:){5}[0-9A-Z]+".toRegex()
+        val devices = regex.findAll(readMessage)
+
+        devices.forEach {
+            val device = Device()
+
+            device.ip = it.value.split("\\s+".toRegex())[0]
+            device.mac = it.value.split("\\s+".toRegex())[1]
+
+            if (!deviceList.contains(device)) deviceList.add(device)
+        }
+
+        return !devices.none()
+    }
+
 }
