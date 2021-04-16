e2a0e6fa0a8026130ab76b3b4df1ba937b40e77e
BryanGazali <46581520+BryanGazali@users.noreply.github.com>
2021-04-16 15:53:43 -0400

    Update NetworkFragment.kt

--- app/src/main/kotlin/io/treehouses/remote/ui/network/NetworkFragment.kt
+++ app/src/main/kotlin/io/treehouses/remote/ui/network/NetworkFragment.kt
@@ -1,8 +1,10 @@
 package io.treehouses.remote.ui.network
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
