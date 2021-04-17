1034725dfc7fa58f9b54ec571c2248176e277df1
BryanGazali <46581520+BryanGazali@users.noreply.github.com>
2021-04-16 23:26:16 -0400

    network: add speedtest button (fixes #1782) (#1783)
    Co-authored-by: roshan <rijalroshan94@gmail.com>

--- app/src/main/kotlin/io/treehouses/remote/ui/network/NetworkViewModel.kt
+++ app/src/main/kotlin/io/treehouses/remote/ui/network/NetworkViewModel.kt
@@ -16,15 +16,14 @@ import io.treehouses.remote.utils.match
 import io.treehouses.remote.pojo.NetworkProfile
 import io.treehouses.remote.utils.*
 
-class NetworkViewModel(application: Application) : FragmentViewModel(application) {
+class NetworkViewModel(application: Application) : BaseNetworkViewModel(application) {
     private val context = getApplication<MainApplication>().applicationContext
     var networkMode: MutableLiveData<String> = MutableLiveData()
     var ipAddress: MutableLiveData<String> = MutableLiveData()
     val reverseText: MutableLiveData<String> = MutableLiveData()
-    var wifiUserError: MutableLiveData<Boolean> = MutableLiveData()
     var showHome: MutableLiveData<Boolean> = MutableLiveData()
-    var showNetworkProgress: MutableLiveData<Boolean> = MutableLiveData()
-    var checkBoxChecked: MutableLiveData<Boolean> = MutableLiveData()
+    val downloadUpload: MutableLiveData<String> = MutableLiveData()
+    var dialogCheck: MutableLiveData<Boolean> = MutableLiveData()
 
     private fun updateNetworkText(mode: String) {
         logD( "Current Network Mode: $mode" )
@@ -74,26 +73,19 @@ class NetworkViewModel(application: Application) : FragmentViewModel(application
         when (match(output)) {
             RESULTS.NETWORKMODE, RESULTS.DEFAULT_NETWORK -> updateNetworkText(output)
             RESULTS.NETWORKMODE_INFO -> showIpAddress(output)
-            RESULTS.DEFAULT_CONNECTED -> {
-                //update network mode
-                getNetworkMode()
-            }
+            RESULTS.DEFAULT_CONNECTED ->  getNetworkMode()
             RESULTS.ERROR -> {
-                // showDialog(context, "Error", output)
                 showNetworkProgress.value = false
             }
             RESULTS.HOTSPOT_CONNECTED, RESULTS.WIFI_CONNECTED, RESULTS.BRIDGE_CONNECTED -> {
-
-                // showDialog(context, "Network Switched", output)
-                //update network mode
                 getNetworkMode()
-                logD("HOTSPOT CONNECTED")
-                // Utils.sendMessage(listener, Pair(msg, "Network Mode retrieved"), context, Toast.LENGTH_LONG)
                 showNetworkProgress.value = false
             }
-            RESULTS.REVERSE_LOOKUP ->{
-                showRemoteReverse(output)
+            RESULTS.BOOLEAN -> updateInternet(output)
+            RESULTS.SPEED_TEST -> {
+                updateSpeed(output)
             }
+            RESULTS.REVERSE_LOOKUP -> showRemoteReverse(output)
             else -> logE("NewNetworkFragment: Result not Found")
         }
 
@@ -112,80 +104,46 @@ class NetworkViewModel(application: Application) : FragmentViewModel(application
 //        reverseText.value = output
     }
 
-     fun getNetworkMode() {
+    override fun onError(output: String) {
+        super.onError(output)
+        downloadUpload.value = "Speed Test Failed"
+        Toast.makeText(context, "Python Error", Toast.LENGTH_LONG).show()
+    }
+
+    fun getNetworkMode() {
         val msg = getString(R.string.TREEHOUSES_NETWORKMODE)
         sendMessage(msg)
     }
 
-
-    fun bridgeStartConfigListener(stringMap: Map<String, String>) {
-        showNetworkProgress.value = true
-        logD(stringMap.getValue("etEssid"));
-        sendMessage(getString(R.string.TREEHOUSES_BRIDGE, stringMap.getValue("etEssid"), stringMap.getValue("etHotspotEssid"),
-                stringMap.getValue("etPassword"), stringMap.getValue("etHotspotPassword")))
-        Toast.makeText(context, "Connecting...", Toast.LENGTH_LONG).show()
+    fun treehousesInternet(){
+        dialogCheck.value = true
+        sendMessage("treehouses internet")
     }
 
-    fun bridgeSetAddProfileListener(stringMap: Map<String, String>) {
-        val networkProfile = NetworkProfile(stringMap.getValue("etEssid"), stringMap.getValue("etHotspotEssid"),
-                stringMap.getValue("etPassword"), stringMap.getValue("etHotspotPassword"))
-        SaveUtils.addProfile(context, networkProfile)
-        Toast.makeText(context, "Bridge Profile Added", Toast.LENGTH_LONG).show()
-    }
-
-    fun ethernetStartConfigListener(ip: String, mask: String, gateway: String, dns: String){
-        sendMessage(getString(R.string.TREEHOUSES_ETHERNET, ip, mask, gateway, dns))
-    }
-
-    fun hotspotStartConfigListener(etHotspotSsid: String, etHotspotPassword: String,
-                                   checkBoxHiddenHotspot: Boolean, spnHotspotType: String) {
-        if (checkBoxHiddenHotspot) sendHotspotMessage(R.string.TREEHOUSES_AP_HIDDEN, spnHotspotType,
-                etHotspotSsid, etHotspotPassword)
-        else sendHotspotMessage(R.string.TREEHOUSES_AP, spnHotspotType, etHotspotSsid, etHotspotPassword)
-        Toast.makeText(context, "Connecting...", Toast.LENGTH_LONG).show()
-    }
-
-
-    private fun sendHotspotMessage(command : Int, spnHotspotType: String, etHotspotSsid: String, etHotspotPassword: String) {
-        showNetworkProgress.value = true
-        sendMessage(getString(command, spnHotspotType,
-                etHotspotSsid, etHotspotPassword))
-
-    }
-
-    fun hotspotSetAddProfileListener(checkBoxHiddenHotspot: Boolean, spnHotspotType: String,
-                                     etHotspotSsid: String, etHotspotPassword: String) {
-        SaveUtils.addProfile(context,
-                NetworkProfile(etHotspotSsid, etHotspotPassword,
-                        spnHotspotType, checkBoxHiddenHotspot))
-        Toast.makeText(context, "Hotspot Profile Saved", Toast.LENGTH_LONG).show()
-    }
-
-
-    fun sendWifiMessage(booleanMap: Map<String, Boolean>, ssid:String, password: String, username: String) {
-        if (booleanMap.getValue("checkBoxEnterprise") && username.isEmpty()) {
-            wifiUserError.value = true
-            return
+    fun updateInternet(output: String){
+        if (output.contains("true")) {
+            downloadUpload.value = "Internet check passed. Performing speed test......"
+            sendMessage("treehouses speedtest")
+        } else{
+            downloadUpload.value = "Internet check failed. Connect to network"
         }
-        wifiUserError.value = false
-        val hidden = booleanMap.getValue("checkBoxHiddenWifi")
-        val enterprise = booleanMap.getValue("checkBoxEnterprise")
-        when {
-            !enterprise -> sendMessage(getString(if (hidden) R.string.TREEHOUSES_WIFI_HIDDEN else R.string.TREEHOUSES_WIFI, ssid, password))
-            enterprise -> sendMessage(getString(if (hidden) R.string.TREEHOUSES_WIFI_HIDDEN_ENTERPRISE else R.string.TREEHOUSES_WIFI_ENTERPRISE, ssid, password, username))
+    }
+
+    fun updateSpeed(output: String){
+        if (output.contains("Download:") && output.contains("Upload:")){
+            downloadUpload.value = getSubString("Download:", output)
+            downloadUpload.value += "\n" + getSubString("Upload", output)
+        } else if (output.contains("Download:")){
+            downloadUpload.value = getSubString("Download:", output)
+        } else {
+            downloadUpload.value += "\n" + getSubString("Upload", output)
         }
-        showNetworkProgress.value = true
-        Toast.makeText(context, "Connecting...", Toast.LENGTH_LONG).show()
     }
 
-    fun wifiSetAddProfileListener(editTextSSID: String, wifipassword: String, checkBoxHiddenWifi: Boolean) {
-        SaveUtils.addProfile(context, NetworkProfile(editTextSSID,
-                wifipassword, checkBoxHiddenWifi))
-        Toast.makeText(context, "WiFi Profile Saved", Toast.LENGTH_LONG).show()
-    }
-
-    fun hiddenOrEnterprise(isChecked: Boolean) {
-        checkBoxChecked.value = isChecked
+    fun getSubString(stringStart: String, output: String) : String {
+        var startIndex = output.indexOf(stringStart)
+        var endIndex = output.indexOf("/s", startIndex)
+        return output.substring(startIndex, endIndex + 2)
     }
 
 }
