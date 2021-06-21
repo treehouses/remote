6540fb5dd0bf1b9cf722a2a7b176d6f0437518e7
BryanGazali <46581520+BryanGazali@users.noreply.github.com>
2021-06-21 12:50:31 -0700

    fix cc

--- app/src/main/kotlin/io/treehouses/remote/ui/sshconfig/SSHConfigFragment.kt
+++ app/src/main/kotlin/io/treehouses/remote/ui/sshconfig/SSHConfigFragment.kt
@@ -102,8 +102,8 @@ class SSHConfigFragment : BaseFragment(), RVButtonClickListener, OnHostStatusCha
             bind.noHosts.visibility = if (!it) View.GONE else View.VISIBLE
         })
         viewModel.pastsHostsVisibility.observe(viewLifecycleOwner, Observer {
-            if (it) bind.pastHosts.visibility = View.VISIBLE
-            else bind.pastHosts.visibility = View.GONE
+            var view = if (it) View.VISIBLE else View.GONE
+            bind.pastHosts.visibility = view
         })
         viewModel.pastHostsList.observe(viewLifecycleOwner, Observer {
             pastHosts = it
