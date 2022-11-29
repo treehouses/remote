6ab087605d20b3c765216afd265ebda165bfe365
BryanGazali <46581520+BryanGazali@users.noreply.github.com>
2022-11-29 10:05:14 +0800

    connect: fix mac address (fixes #1806)

--- app/src/main/kotlin/io/treehouses/remote/fragments/dialogfragments/RPIDialogFragment.kt
+++ app/src/main/kotlin/io/treehouses/remote/fragments/dialogfragments/RPIDialogFragment.kt
@@ -237,11 +237,11 @@ class RPIDialogFragment : BaseDialogFragment(), DeviceDeleteListener {
 
         fun checkPiAddress(deviceHardwareAddress: String): Boolean {
             val piAddress: Set<String> = HashSet(listOf("B8:27:EB", "DC:A6:32", "E4:5F:01",
-                    "B8-27-EB", "DC-A6-32", "E4-5F-01",
-                    "B827.EB", "DCA6.32", "E45F.01",
-                    "b8:27:eb", "dc:a6:32", "e4:5f:01",
-                    "b8-27-eb", "dc-a6-32", "E4-5F-01",
-                    "b827.eb", "dca6.32", "E45F.01"))
+                                                        "B8-27-EB", "DC-A6-32", "E4-5F-01",
+                                                        "B827.EB", "DCA6.32", "E45F.01",
+                                                        "b8:27:eb", "dc:a6:32", "e4:5f:01",
+                                                        "b8-27-eb", "dc-a6-32", "e4-5f-01",
+                                                        "b827.eb", "dca6.32", "e45f.01"))
             for (item in piAddress) {
                 if (deviceHardwareAddress.contains(item)) return true
             }
