4ae8bdcf18dd25ada8b570bbd5b1df4c6160cbb9
Gideon Okuro <gideonollonde@gmail.com>
2024-06-05 12:41:26 +0300

    check if gps is enabled before accessing location

--- app/src/main/kotlin/io/treehouses/remote/utils/GPSService.kt
+++ app/src/main/kotlin/io/treehouses/remote/utils/GPSService.kt
@@ -52,11 +52,12 @@ open class GPSService : Service(), LocationListener {
             .build()
 
         startForeground(NOTIFICATION_ID, notification)
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        isGPSEnabled = locationManager?.isProviderEnabled(LocationManager.GPS_PROVIDER) == true
+
+        if (isGPSEnabled && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
             && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
             && ActivityCompat.checkSelfPermission(this, Manifest.permission.FOREGROUND_SERVICE) == PackageManager.PERMISSION_GRANTED) {
-            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            isGPSEnabled = locationManager?.isProviderEnabled(LocationManager.GPS_PROVIDER) == true
             getLocation()
         } else {
             stopSelf()
